7a69a5a8c6d8d246d2bd442f8f6d87a7f4fd889c
Waleed Tariq <dev.ratech@outlook.com>
2022-02-08 10:43:27 +0500

    add crud service interface

---
+++ Application.Contracts/Interfaces/ICrudService.cs
@@ -0,0 +1,20 @@
+﻿using Application.Contracts.Response;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Interfaces
+{
+    public interface ICrudService<VM, DTO, TKey>
+    {
+        Task<Response<List<DTO>>> GetAllAsync();
+        Task<Response<DTO>> GetByIdAsync(TKey id);
+        Task<Response<DTO>> CreateAsync(VM entity);
+        Task<Response<DTO>> UpdateAsync(VM entity);
+        Task<Response<TKey>> DeleteAsync(TKey id);
+        IEnumerable<DTO> Find();
+    }
+}
