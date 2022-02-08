00f2b3291f68e445fa9b2ba92ac2f69e58d5a26f
Waleed Tariq <dev.ratech@outlook.com>
2022-02-08 10:48:02 +0500

    define UoW class

---
+++ Infrastructure/Uow/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Context;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Uow
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly ApplicationDbContext _context;
+        private IDbContextTransaction _transaction;
+        public IClientRepository Client { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            Client = new ClientRepository(context);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void CreateTransaction()
+        {
+            _transaction = _context.Database.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            _transaction.Commit();
+        }
+
+        public void Rollback()
+        {
+            _transaction.Rollback();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
