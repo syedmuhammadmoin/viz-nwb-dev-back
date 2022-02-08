f22ce755b61e15bb8a15b899e8062d469574ef5c
Waleed Tariq <dev.ratech@outlook.com>
2022-02-08 10:43:09 +0500

    add crud service interface

---
+++ Application.Contracts/Interfaces/IClientService.cs
@@ -0,0 +1,13 @@
+﻿using Application.Contracts.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Interfaces
+{
+    public interface IClientService : ICrudService<CreateClientDto, ClientDto, int>
+    {
+    }
+}
