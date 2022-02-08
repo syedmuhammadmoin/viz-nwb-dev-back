664189de81b11b9b239aa611160f4d9df7873ebe
Waleed Tariq <dev.ratech@outlook.com>
2022-02-08 10:44:32 +0500

    change baseEntity T name

--- Domain/Base/BaseEntity.cs
+++ Domain/Base/BaseEntity.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Base
 {
-    public abstract class BaseEntity<T>
+    public abstract class BaseEntity<TKey>
     {
-        public T Id { get; set; }
+        public TKey Id { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? ModifiedDate { get; set; }
+        [MaxLength(100)]
         public string CreatedBy { get; set; }
+        [MaxLength(100)]
         public string ModifiedBy { get; set; }
         public bool IsDelete { get; set; }
     }
