768952032ead4a5333a9a7e3f54a8b6185b02eee
Waleed Tariq <dev.ratech@outlook.com>
2022-02-08 10:47:51 +0500

    add client specification

---
+++ Infrastructure/Specifications/ClientWithSpecificName.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Infrastructure.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Specifications
+{
+    public class ClientWithSpecificName : BaseSpecification<Client>
+    {
+        public ClientWithSpecificName(string name) : base(x => x.Name.Contains(name))
+        {
+
+        }
+        public ClientWithSpecificName()
+        {
+
+        }
+    }
+}
