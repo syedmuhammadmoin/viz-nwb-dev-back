97b7d4f4a241d4be01aa0e7f526abb0ef5d4f3ec
Waleed Tariq <dev.ratech@outlook.com>
2022-02-08 10:49:34 +0500

    add services and automapper configuration

--- VizalysDDD/Program.cs
+++ VizalysDDD/Program.cs
@@ -1,12 +1,19 @@
+
+using Application.Contracts.Interfaces;
+using Application.Services;
+using Domain.Interfaces;
 using Infrastructure;
 using Infrastructure.Seeds;
+using Infrastructure.Uow;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddInfrastructure(builder.Configuration);
-
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IClientService, ClientService>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
