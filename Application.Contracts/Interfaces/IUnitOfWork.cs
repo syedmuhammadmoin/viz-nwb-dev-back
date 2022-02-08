d0e88e208f8a3ed21d69ae72ce62dc829aeff101
Waleed Tariq <dev.ratech@outlook.com>
2022-02-08 10:43:40 +0500

    add UoW interface

---
+++ Application.Contracts/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IClientRepository Client { get; }
+        Task SaveAsync();
+        void CreateTransaction();
+        void Commit();
+        void Rollback();
+        void Dispose();
+    }
+}
