d38c311171462eb1cbfe32141d01b6367e5c92fd
Waleed Tariq <dev.ratech@outlook.com>
2022-02-08 10:41:02 +0500

    add  auto mapper profile

---
+++ Application/Mapper/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using Application.Contracts.DTOs;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Client, ClientDTO>();
+            CreateMap<CreateClientDto, Client>();
+        }
+    }
+}
