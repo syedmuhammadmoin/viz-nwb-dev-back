648ec8f974b5845706e1d951b1dbe93c0251e84c
Waleed Tariq <dev.ratech@outlook.com>
2022-02-08 10:42:32 +0500

    add client dtos

---
+++ Application.Contracts/DTOs/Clients/ClientDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class ClientDto
+    {
+        public int Id { get; set; }
+        public string Name { get; private set; }
+        public string Country { get; private set; }
+        public string State { get; private set; }
+        public string City { get; private set; }
+        public string Address { get; private set; }
+        public string Phone { get; private set; }
+        public string Fax { get; private set; }
+        public string Email { get; private set; }
+        public string Website { get; private set; }
+        public string BankName { get; private set; }
+        public string BankAccountTitle { get; private set; }
+        public string BankAccountNumber { get; private set; }
+        public string Currency { get; private set; }
+    }
+}
---
+++ Application.Contracts/DTOs/Clients/CreateClientDto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class CreateClientDto
+    {
+        public int? Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; private set; }
+        [MaxLength(50)]
+        public string Country { get; private set; }
+        [MaxLength(50)]
+        public string State { get; private set; }
+        [MaxLength(50)]
+        public string City { get; private set; }
+        [MaxLength(20)]
+        public string Address { get; private set; }
+        [MaxLength(20)]
+        public string Phone { get; private set; }
+        [MaxLength(20)]
+        public string Fax { get; private set; }
+        [MaxLength(20)]
+        public string Email { get; private set; }
+        [MaxLength(50)]
+        public string Website { get; private set; }
+        [MaxLength(50)]
+        public string BankName { get; private set; }
+        [MaxLength(50)]
+        public string BankAccountTitle { get; private set; }
+        [MaxLength(30)]
+        public string BankAccountNumber { get; private set; }
+        [MaxLength(30)]
+        public string Currency { get; private set; }
+    }
+}
