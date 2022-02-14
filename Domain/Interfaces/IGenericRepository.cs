8ac999a06e190f46c2cbe7d4d57dbc008384eec8
Waleed Tariq <dev.ratech@outlook.com>
2022-02-14 17:05:16 +0500

    add total record method and specifications

--- Domain/Interfaces/IGenericRepository.cs
+++ Domain/Interfaces/IGenericRepository.cs
@@ -10,10 +10,11 @@ namespace Domain.Interfaces
 {
     public interface IGenericRepository<T, TKey> where T : BaseEntity<TKey>
     {
-        Task<IEnumerable<T>> GetAll();
-        Task<T> GetById(TKey id);
+        Task<IEnumerable<T>> GetAll(ISpecification<T> specification = null);
+        Task<T> GetById(TKey id, ISpecification<T> specification = null);
         Task<T> Add(T entity);
         Task<bool> Delete(TKey id);
+        Task<int> TotalRecord();
         IEnumerable<T> Find(ISpecification<T> specification = null);
     }
 }
