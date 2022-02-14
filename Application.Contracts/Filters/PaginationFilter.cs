8b354e76978b5783482ee1328291566aba6e446a
Waleed Tariq <dev.ratech@outlook.com>
2022-02-14 17:00:39 +0500

    add pagination filter

---
+++ Application.Contracts/Filters/PaginationFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Filters
+{
+    public class PaginationFilter
+    {
+        public int PageStart { get; set; }
+        public int PageEnd { get; set; }
+        public PaginationFilter()
+        {
+            this.PageStart = 0;
+            this.PageEnd = 20;
+        }
+        public PaginationFilter(int pageStart, int pageEnd)
+        {
+            this.PageStart = pageStart;
+            this.PageEnd = pageEnd;
+        }
+    }
+}
