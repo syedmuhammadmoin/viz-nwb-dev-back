3e297ae449d83cb2432eb49cdd821b4dbc521f8c
Waleed Tariq <dev.ratech@outlook.com>
2022-02-14 17:00:30 +0500

    add org dtos

---
+++ Application.Contracts/DTOs/Organizations/CreateOrganizationDto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class CreateOrganizationDto
+    {
+        public int? Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(500)]
+        public string Description { get; set; }
+        [MaxLength(50)]
+        public string Country { get; set; }
+        [MaxLength(50)]
+        public string State { get; set; }
+        [MaxLength(50)]
+        public string City { get; set; }
+        [MaxLength(200)]
+        public string Address { get; set; }
+        [MaxLength(20)]
+        public string Phone { get; set; }
+        [MaxLength(20)]
+        public string Fax { get; set; }
+        [MaxLength(20)]
+        public string Email { get; set; }
+        [MaxLength(50)]
+        public string Website { get; set; }
+        //Industry will link from chart of account in future...
+        [MaxLength(50)]
+        public string Industry { get; set; }
+        //this will link from tax in future
+        [MaxLength(50)]
+        public string LegalStatus { get; set; }
+        [MaxLength(50)]
+        public string IncomeTaxId { get; set; }
+        [MaxLength(50)]
+        public string SalesTaxId { get; set; }
+        [Required]
+
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+        [Required]
+        public int ClientId { get; set; }
+    }
+}
---
+++ Application.Contracts/DTOs/Organizations/OrganizationDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class OrganizationDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string Website { get; set; }
+        public string Industry { get; set; }
+        public string LegalStatus { get; set; }
+        public string IncomeTaxId { get; set; }
+        public string SalesTaxId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int ClientId { get; set; }
+        public string ClientName { get; set; }
+    }
+}
