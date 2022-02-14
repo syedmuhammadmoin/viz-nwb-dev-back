099e0029add5ebddc70ddc837dd3c79a7c7bfec4
Waleed Tariq <dev.ratech@outlook.com>
2022-02-14 17:03:19 +0500

    add pagination response

---
+++ Application.Contracts/Response/PaginationResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Response
+{
+    public class PaginationResponse<T> : Response<T>
+    {
+        public int PageStart { get; private set; }
+        public int PageEnd { get; private set; }
+        public int TotalRecords { get; private set; }
+
+        public PaginationResponse(T data, int pageStart, int pageEnd, int totalRecords, string message)
+        {
+            this.PageStart = pageStart;
+            this.PageEnd = pageEnd;
+            this.TotalRecords = totalRecords;
+            this.Result = data;
+            this.Message = message;
+            this.IsSuccess = true;
+        }
+        public PaginationResponse(string message)
+        {
+            this.Message = message;
+            this.IsSuccess = false;
+        }
+    }
+}
