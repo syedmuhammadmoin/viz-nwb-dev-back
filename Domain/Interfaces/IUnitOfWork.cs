9b339a539a69dd687bbd94ebf23530522f6b8d72
Waleed Tariq <dev.ratech@outlook.com>
2022-02-14 17:05:29 +0500

    add unit of work in domain layer

---
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IClientRepository Client { get; }
+        IOrganizationRepository Organization { get; }
+
+        Task SaveAsync();
+        void CreateTransaction();
+        void Commit();
+        void Rollback();
+        void Dispose();
+    }
+}
