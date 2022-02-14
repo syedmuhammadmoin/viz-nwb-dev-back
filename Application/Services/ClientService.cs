323cb87b65ae726942e10c83e2100c905c073ee6
Waleed Tariq <dev.ratech@outlook.com>
2022-02-14 16:55:36 +0500

    add specification and pagination in client service

--- Application/Services/ClientService.cs
+++ Application/Services/ClientService.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Contracts.DTOs;
+using Application.Contracts.Filters;
 using Application.Contracts.Interfaces;
 using Application.Contracts.Response;
 using Application.Specifications;
@@ -24,54 +25,53 @@ namespace Application.Services
             _mapper = mapper;
         }
 
-        public async Task<Response<List<ClientDto>>> GetAllAsync()
-        {
-            var clients = await _unitOfWork.Client.GetAll();
-            return new Response<List<ClientDto>>(_mapper.Map<List<ClientDto>>(clients), "Returing clients list");
-        }
-
-        public async Task<Response<ClientDto>> GetByIdAsync(int id)
-        {
-            var client = await _unitOfWork.Client.GetById(id);
-            if (client == null)
-                return new Response<ClientDto>("Client not found");
-
-            return new Response<ClientDto>(_mapper.Map<ClientDto>(client), "Returning client");
-        }
-
         public async Task<Response<ClientDto>> CreateAsync(CreateClientDto entity)
         {
             var client = new Client(_mapper.Map<Client>(entity));
             var result = await _unitOfWork.Client.Add(client);
             await _unitOfWork.SaveAsync();
 
-            return new Response<ClientDto>(_mapper.Map<ClientDto>(result), "Client created successfully");
+            return new Response<ClientDto>(_mapper.Map<ClientDto>(result), "Created successfully");
+        }
+
+        public async Task<PaginationResponse<List<ClientDto>>> GetAllAsync(PaginationFilter filter)
+        {
+            var specification = new ClientSpecs(filter);
+            var clients = await _unitOfWork.Client.GetAll(specification);
+
+            if (clients.Count() == 0)
+                return new PaginationResponse<List<ClientDto>>("List is empty");
+
+            var totalRecords = await _unitOfWork.Client.TotalRecord();
+
+            return new PaginationResponse<List<ClientDto>>(_mapper.Map<List<ClientDto>>(clients), filter.PageStart, filter.PageEnd, totalRecords, "Returing list");
+        }
+
+        public async Task<Response<ClientDto>> GetByIdAsync(int id)
+        {
+            var client =  await _unitOfWork.Client.GetById(id);
+            if (client == null)
+                return new Response<ClientDto>("Not found");
+
+            return new Response<ClientDto>(_mapper.Map<ClientDto>(client), "Returning value");
         }
 
         public async Task<Response<ClientDto>> UpdateAsync(CreateClientDto entity)
         {
             var client = await _unitOfWork.Client.GetById((int)entity.Id);
-            
+
             if (client == null)
-                return new Response<ClientDto>("Client not found");
+                return new Response<ClientDto>("Not found");
 
             //For updating data
             _mapper.Map<CreateClientDto, Client>(entity, client);
             await _unitOfWork.SaveAsync();
-            return new Response<ClientDto>(_mapper.Map<ClientDto>(client), "Client created successfully");
+            return new Response<ClientDto>(_mapper.Map<ClientDto>(client), "Updated successfully");
         }
 
         public Task<Response<int>> DeleteAsync(int id)
         {
             throw new NotImplementedException();
-        }
-
-        public IEnumerable<ClientDto> Find()
-        {
-            var specification = new ClientWithSpecificName("Hamza");
-            //var specification = new DeveloperByIncomeSpecification();
-            var client = _unitOfWork.Client.Find(specification);
-            return _mapper.Map<IEnumerable<ClientDto>>(client);
         }
     }
 }
