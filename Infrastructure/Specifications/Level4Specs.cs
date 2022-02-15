471f1ee2250745bf94f72bd4aac8f0442a554626
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-02-15 19:39:20 +0500

    Added Specs for Level4, Product, Category, BusinessPartner

---
+++ Infrastructure/Specifications/Level4Specs.cs
@@ -0,0 +1,19 @@
+﻿using Application.Contracts.Filters;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Specifications
+{
+    public class Level4Specs : BaseSpecification<Level4>
+    {
+        public Level4Specs(PaginationFilter filter)
+        {
+            var validFilter = new PaginationFilter(filter.PageStart, filter.PageEnd);
+            ApplyPaging(validFilter.PageStart, validFilter.PageEnd - validFilter.PageStart);
+        }
+    }
+}
