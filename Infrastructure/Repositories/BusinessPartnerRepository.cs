b8f0468f71dba291cc1786f9f530b84365edf1e5
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-02-15 19:40:01 +0500

    Added Repositories for Level4, Product, Category, BusinessPartner

---
+++ Infrastructure/Repositories/BusinessPartnerRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class BusinessPartnerRepository : GenericRepository<BusinessPartner, int>, IBusinessPartnerRepository
+    {
+        public BusinessPartnerRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ Infrastructure/Repositories/Level2Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Context;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class Level2Repository : GenericRepository<Level2, Guid>, ILevel2Repository
+    {
+        public Level2Repository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
