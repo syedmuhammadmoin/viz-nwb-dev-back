917080ae6e8c7faed85754d86d351098a4fce2d4
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-02-15 19:40:14 +0500

    updated mapping profile for Level4, Product, Category, BusinessPartner

--- Application/Mapper/MappingProfile.cs
+++ Application/Mapper/MappingProfile.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Contracts.DTOs;
+using Application.Contracts.DTOs.Products;
 using AutoMapper;
 using Domain.Entities;
 using System;
@@ -19,6 +20,22 @@ namespace Application.Mapper
             CreateMap<Organization, OrganizationDto>()
                 .ForMember(dto => dto.ClientName, core => core.MapFrom(a => a.Client.Name));
             CreateMap<CreateOrganizationDto, Organization>();
+
+            CreateMap<Level4, Level4Dto>()
+                .ForMember(dto => dto.Name, core => core.MapFrom(a => a.Name));
+            CreateMap<CreateLevel4Dto, Level4>();
+
+            CreateMap<Category, CategoryDto>()
+                .ForMember(dto => dto.Name, core => core.MapFrom(a => a.Name));
+            CreateMap<CreateCategoryDto, Category>();
+
+            CreateMap<BusinessPartner, BusinessPartnerDto>()
+                .ForMember(dto => dto.Name, core => core.MapFrom(a => a.Name));
+            CreateMap<CreateBusinessPartnerDto, BusinessPartner>();
+
+            CreateMap<Product, ProductDto>()
+                .ForMember(dto => dto.ProductName, core => core.MapFrom(a => a.ProductName));
+            CreateMap<CreateProductDto, Product>();
         }
     }
 }
