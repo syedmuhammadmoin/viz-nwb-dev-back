a830fac508f425ee57b585955086ae320d653b03
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-02-15 19:41:19 +0500

    IServices for Level4, Product, Category, BusinessPartner

--- Application.Contracts/Interfaces/IApplicationDbContext.cs
+++ Application.Contracts/Interfaces/IApplicationDbContext.cs
@@ -12,6 +12,10 @@ namespace Application.Interfaces
     {
         public DbSet<Client> Clients { get; set; }
         public DbSet<Organization> Organizations { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Level4> Level4 { get; set; }
+        public DbSet<Product> Products { get; set; }
+
         Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
