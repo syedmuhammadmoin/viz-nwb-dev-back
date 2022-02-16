89c5602132986d186dbdec7bae6983227f2fc59d
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-02-16 19:12:15 +0500

    added controller for JournalEntry

---
+++ VizalysDDD/Controllers/JournalEntryController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Contracts.DTOs;
+using Application.Contracts.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vizalys.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JournalEntryController : ControllerBase
+    {
+        private readonly IJournalEntryService _journalEntryService;
+
+        public JournalEntryController(IJournalEntryService journalEntryService)
+        {
+            _journalEntryService = journalEntryService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<JournalEntryDto>> CreateAsync(CreateJournalEntryDto entity)
+        {
+            var journalEntry = await _journalEntryService.CreateAsync(entity);
+            return Ok(journalEntry);
+        }
+
+    }
+}
