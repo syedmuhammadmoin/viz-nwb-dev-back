bc616b7557df065167acdd807ab2c117c4f2f7cb
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-02-16 19:10:35 +0500

    Added JournalEntry Dto

---
+++ Application.Contracts/DTOs/JournalEntry/JournalEntryDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class JournalEntryDto
+    {
+        public decimal TotalDebit { get; set; }
+        public decimal TotalCredit { get; set; }
+        public virtual IEnumerable<JournalEntryLinesDto> JournalEntryLines { get; set; }
+        public bool IsAllowedRole { get; set; }
+    }
+}
