d5df39a579c82036a7f46dad8be5122d14b2e349
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-02-16 19:11:49 +0500

    specificaiton for JournalEntry

---
+++ Infrastructure/Specifications/JournalEntryLinesSpecs.cs
@@ -0,0 +1,20 @@
+﻿using Application.Contracts.Filters;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Specifications
+{
+    public class JournalEntryLinesSpecs : BaseSpecification<JournalEntryLines>
+    {
+        public JournalEntryLinesSpecs(PaginationFilter filter)
+        {
+            var validFilter = new PaginationFilter(filter.PageStart, filter.PageEnd);
+            ApplyPaging(validFilter.PageStart, validFilter.PageEnd - validFilter.PageStart);
+        }
+    }
+
+}
