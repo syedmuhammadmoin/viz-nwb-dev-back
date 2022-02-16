31c9ebdba6f0f45f4c4807bea486f0601308ab22
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-02-16 11:17:08 +0500

    fixed namepace in ProductyDto

--- Application.Contracts/DTOs/Products/CreateProductDto.cs
+++ Application.Contracts/DTOs/Products/CreateProductDto.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.Contracts.DTOs.Products
+namespace Application.Contracts.DTOs
 {
     public class CreateProductDto
     {
--- Application.Contracts/DTOs/Products/ProductDto.cs
+++ Application.Contracts/DTOs/Products/ProductDto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.Contracts.DTOs.Products
+namespace Application.Contracts.DTOs
 {
     public class ProductDto
     {
