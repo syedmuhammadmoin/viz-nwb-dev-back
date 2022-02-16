25c614dd8d2337b7508a3864c5f0ecdb7ed8ab83
Waleed Tariq <dev.ratech@outlook.com>
2022-02-16 15:10:42 +0500

    add level4 dto

---
+++ Application.Contracts/DTOs/Level4/Level4Dto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class Level4Dto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid Level3_id { get; set; }
+        public string Level3Name { get; set; }
+    }
+}
