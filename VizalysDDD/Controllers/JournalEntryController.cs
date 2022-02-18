e3acee711d9f151c6829445768ec172277272a75
Waleed Tariq <dev.ratech@outlook.com>
2022-02-18 14:27:02 +0500

    add jv controller

--- VizalysDDD/Controllers/JournalEntryController.cs
+++ VizalysDDD/Controllers/JournalEntryController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Contracts.DTOs;
+using Application.Contracts.Filters;
 using Application.Contracts.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -21,6 +22,30 @@ namespace Vizalys.Api.Controllers
         {
             var journalEntry = await _journalEntryService.CreateAsync(entity);
             return Ok(journalEntry);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<JournalEntryDto>>> GetAllAsync([FromQuery] PaginationFilter filter)
+        {
+            var jvs = await _journalEntryService.GetAllAsync(filter);
+            return Ok(jvs);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<JournalEntryDto>> GetByIdAsync(int id)
+        {
+            var result = await _journalEntryService.GetByIdAsync(id);
+            return Ok(result); // Status Code : 200
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<JournalEntryDto>> UpdateAsync(int id, CreateJournalEntryDto entity)
+        {
+            if (id != entity.Id)
+                return BadRequest("ID mismatch");
+
+            var result = await _journalEntryService.UpdateAsync(entity);
+            return Ok(result); // Status Code : 200
         }
 
     }
