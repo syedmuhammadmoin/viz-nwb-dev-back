2eecb864a27d8ebcc0a75178c36c42f86e3f8863
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-02-18 20:45:48 +0500

    Added InvoiceMaster and InvoiceLine Entities

---
+++ Domain/Entities/InvoiceMaster.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Base;
+using Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class InvoiceMaster : BaseEntity<int>
+    {
+        public int CustomerId { get; set; }
+        [ForeignKey("CustomerId")]
+        public BusinessPartner Customer { get; set; }
+        [MaxLength(20)]
+        public string DocNo { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public DateTime DueDate { get; set; }
+        [MaxLength(20)]
+        public string Contact { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TotalBeforeTax { get; private set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TotalTax { get; private set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TotalAmount { get; private set; }
+        public DocumentStatus Status { get; set; }
+        public virtual List<InvoiceLines> InvoiceLines { get; set; }
+        
+        
+        protected InvoiceMaster()
+        {
+        }
+
+        public void setStatus(DocumentStatus status)
+        {
+            Status = status;
+        }
+
+        public void CreateDocNo()
+        {
+            //Creating doc no..
+            DocNo = "INV-" + String.Format("{0:000}", Id);
+        }
+    }
+}
