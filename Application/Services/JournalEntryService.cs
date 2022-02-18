f9257318dbf064713637fd9f16454038efcb6f20
Waleed Tariq <dev.ratech@outlook.com>
2022-02-18 14:16:19 +0500

    add remaining methods in jv service

--- Application/Services/JournalEntryService.cs
+++ Application/Services/JournalEntryService.cs
@@ -3,8 +3,10 @@ using Application.Contracts.Filters;
 using Application.Contracts.Interfaces;
 using Application.Contracts.Response;
 using AutoMapper;
+using Domain.Constants;
 using Domain.Entities;
 using Domain.Interfaces;
+using Infrastructure.Specifications;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,40 +25,101 @@ namespace Application.Services
             _unitOfWork = unitOfWork;
             _mapper = mapper;
         }
+
         public async Task<Response<JournalEntryDto>> CreateAsync(CreateJournalEntryDto entity)
         {
+            if (entity.isSubmit)
+            {
+                return await this.SubmitJV(entity);
+            }
+            else
+            {
+                return await this.SaveJV(entity, DocumentStatus.Submitted);
+            }
+        }
+
+        public async Task<Response<JournalEntryDto>> UpdateAsync(CreateJournalEntryDto entity)
+        {
+            if (entity.isSubmit)
+            {
+                return await this.SubmitJV(entity);
+            }
+            else
+            {
+                return await this.UpdateJV(entity, DocumentStatus.Draft);
+            }
+        }
+
+        public async Task<PaginationResponse<List<JournalEntryDto>>> GetAllAsync(PaginationFilter filter)
+        {
+            var specification = new JournalEntrySpecs(filter);
+            var jvs = await _unitOfWork.JournalEntry.GetAll(specification);
+
+            if (jvs.Count() == 0)
+                return new PaginationResponse<List<JournalEntryDto>>("List is empty");
+
+            var totalRecords = await _unitOfWork.JournalEntry.TotalRecord();
+
+            return new PaginationResponse<List<JournalEntryDto>>(_mapper.Map<List<JournalEntryDto>>(jvs),
+                filter.PageStart, filter.PageEnd, totalRecords, "Returing list");
+        }
+
+        public async Task<Response<JournalEntryDto>> GetByIdAsync(int id)
+        {
+            var specification = new JournalEntrySpecs();
+            var jv = await _unitOfWork.JournalEntry.GetById(id, specification);
+            if (jv == null)
+                return new Response<JournalEntryDto>("Not found");
+
+            return new Response<JournalEntryDto>(_mapper.Map<JournalEntryDto>(jv), "Returning value");
+        }
+
+        public Task<Response<int>> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private async Task<Response<JournalEntryDto>> SubmitJV(CreateJournalEntryDto entity)
+        {
+            if (entity.Id == null)
+            {
+                return await this.SaveJV(entity, DocumentStatus.Submitted);
+            }
+            else
+            {
+                return await this.UpdateJV(entity, DocumentStatus.Submitted);
+            }
+        }
+
+        private async Task<Response<JournalEntryDto>> SaveJV(CreateJournalEntryDto entity, DocumentStatus status)
+        {
+            if (entity.JournalEntryLines.Count() == 0)
+                return new Response<JournalEntryDto>("Lines are required");
+
+            var jv = _mapper.Map<JournalEntryMaster>(entity);
+
+            if (jv.TotalDebit != jv.TotalCredit)
+                return new Response<JournalEntryDto>("Sum of debit and credit must be equal");
+
+            //Setting status
+            jv.setStatus(status);
+
             _unitOfWork.CreateTransaction();
             try
             {
-                //List<JournalEntryLines> journalEntryLines = entity.JournalEntryLines
-                //    .Select(line => new JournalEntryLines
-                //    {
-                //        AccountId = line.AccountId,
-                //        BusinessPartnerId = line.BusinessPartnerId,
-                //        Description = line.Description,
-                //        Debit = line.Debit,
-                //        Credit = line.Credit,
-                //        LocationId = line.LocationId
-                //    }).ToList();
-
-                List<JournalEntryLines> journalEntryLines = _mapper.Map<List<JournalEntryLines>>(entity.JournalEntryLines); 
-
-                var jv = _mapper.Map<JournalEntryMaster>(entity);
-                jv.JournalEntryLines = journalEntryLines;
-
+                //Saving in table
                 var result = await _unitOfWork.JournalEntry.Add(jv);
                 await _unitOfWork.SaveAsync();
 
-                ////Creating doc no..
-                //string docNo = "JV-" + String.Format("{0:000}", jv.Id);
+                //For creating docNo
+                jv.CreateDocNo();
+                await _unitOfWork.SaveAsync();
 
-                ////For updating docNo
-                //jv.DocNo = docNo;
-                //await context.SaveChangesAsync();
-
+                //Commiting the transaction
                 _unitOfWork.Commit();
 
-
+                //returning response
                 return new Response<JournalEntryDto>(_mapper.Map<JournalEntryDto>(result), "Created successfully");
             }
             catch (Exception ex)
@@ -64,26 +127,50 @@ namespace Application.Services
                 _unitOfWork.Rollback();
                 return new Response<JournalEntryDto>(ex.Message);
             }
-
         }
 
-        public Task<PaginationResponse<List<JournalEntryDto>>> GetAllAsync(PaginationFilter filter)
+        private async Task<Response<JournalEntryDto>> UpdateJV(CreateJournalEntryDto entity, DocumentStatus status)
         {
-            throw new NotImplementedException();
+            if (entity.JournalEntryLines.Count() == 0)
+                return new Response<JournalEntryDto>("Lines are required");
+
+            var totalDebit = entity.JournalEntryLines.Sum(i => i.Debit);
+            var totalCredit = entity.JournalEntryLines.Sum(i => i.Credit);
+
+            if (totalDebit != totalCredit)
+                return new Response<JournalEntryDto>("Sum of debit and credit must be equal");
+
+            var specification = new JournalEntrySpecs();
+            var jv = await _unitOfWork.JournalEntry.GetById((int)entity.Id, specification);
+
+            if (jv == null)
+                return new Response<JournalEntryDto>("Not found");
+
+            if (jv.Status == DocumentStatus.Submitted)
+                return new Response<JournalEntryDto>("Journal voucher already submitted");
+
+            jv.setStatus(status);
+
+            _unitOfWork.CreateTransaction();
+            try
+            {
+                //For updating data
+                _mapper.Map<CreateJournalEntryDto, JournalEntryMaster>(entity, jv);
+
+                await _unitOfWork.SaveAsync();
+
+                //Commiting the transaction
+                _unitOfWork.Commit();
+
+                //returning response
+                return new Response<JournalEntryDto>(_mapper.Map<JournalEntryDto>(jv), "Created successfully");
+            }
+            catch (Exception ex)
+            {
+                _unitOfWork.Rollback();
+                return new Response<JournalEntryDto>(ex.Message);
+            }
         }
 
-        public Task<Response<JournalEntryDto>> GetByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Response<JournalEntryDto>> UpdateAsync(CreateJournalEntryDto entity)
-        {
-            throw new NotImplementedException();
-        }
-        public Task<Response<int>> DeleteAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
