8ffaeb6c9cfa0f6a639899008447a09f09b46729
Waleed Tariq <dev.ratech@outlook.com>
2022-02-18 14:26:34 +0500

    add journal entry spec

--- Infrastructure/Specifications/JournalEntrySpecs.cs
+++ Infrastructure/Specifications/JournalEntrySpecs.cs
@@ -14,6 +14,16 @@ namespace Infrastructure.Specifications
         {
             var validFilter = new PaginationFilter(filter.PageStart, filter.PageEnd);
             ApplyPaging(validFilter.PageStart, validFilter.PageEnd - validFilter.PageStart);
+            AddInclude("JournalEntryLines.BusinessPartner");
+            AddInclude("JournalEntryLines.Account");
+            AddInclude("JournalEntryLines.Location");
+        }
+
+        public JournalEntrySpecs()
+        {
+            AddInclude("JournalEntryLines.BusinessPartner");
+            AddInclude("JournalEntryLines.Account");
+            AddInclude("JournalEntryLines.Location");
         }
     }
 }
