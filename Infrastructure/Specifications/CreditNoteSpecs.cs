6607b7c0fe9b4b964cfabc643a9cf36af73ae810
Ashir Azeem <ashir.azeem@ratechnologies.biz>
2022-02-21 00:41:52 +0500

    Added CreditNote Specs

---
+++ Infrastructure/Specifications/CreditNoteSpecs.cs
@@ -0,0 +1,39 @@
+﻿using Application.Contracts.Filters;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Specifications
+{
+    public class CreditNoteSpecs : BaseSpecification<CreditNoteMaster>
+    {
+        public CreditNoteSpecs(PaginationFilter filter)
+        {
+            var validFilter = new PaginationFilter(filter.PageStart, filter.PageEnd);
+            ApplyPaging(validFilter.PageStart, validFilter.PageEnd - validFilter.PageStart);
+            AddInclude(i => i.Customer);
+            AddInclude("CreditNoteLines.Account");
+            AddInclude("CreditNoteLines.Location");
+            AddInclude("CreditNoteLines.Item");
+        }
+
+        public CreditNoteSpecs(bool forEdit)
+        {
+            if (forEdit)
+            {
+                AddInclude(i => i.CreditNoteLines);
+            }
+            else
+            {
+                AddInclude(i => i.Customer);
+                AddInclude("CreditNoteLines.Account");
+                AddInclude("CreditNoteLines.Location");
+                AddInclude("CreditNoteLines.Item");
+            }
+
+        }
+    }
+}
