e1edfdb911c15d345116d0aad8af796550aa57fe
Ashir Azeem <ashir.azeem@ratechnologies.biz>
2022-02-21 00:39:25 +0500

    Added CreditNoteRepo

---
+++ Infrastructure/Repositories/CreditNoteRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class CreditNoteRepository : GenericRepository<CreditNoteMaster, int>, ICreditNoteRepository
+    {
+        public CreditNoteRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+    }
+}
