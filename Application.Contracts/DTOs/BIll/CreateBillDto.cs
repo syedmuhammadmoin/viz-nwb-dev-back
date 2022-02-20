683eca8104406a7710a6a39b799481420d0badbe
Ashir Azeem <ashir.azeem@ratechnologies.biz>
2022-02-20 21:53:02 +0500

    Added Bill Dto

---
+++ Application.Contracts/DTOs/BIll/CreateBillDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class CreateBillDto
+    {
+        public int? Id { get; set; }
+        [Required]
+        public int VendorId { get; set; }
+        [Required]
+        public string VendorBillRef { get; set; }
+        [Required]
+        public DateTime BillDate { get; set; }
+        [Required]
+        public DateTime DueDate { get; set; }
+        public string Contact { get; set; }
+        [Required]
+        public bool isSubmit { get; set; }
+        public virtual List<CreateBillLinesDto> BillLines { get; set; }
+    }
+}
