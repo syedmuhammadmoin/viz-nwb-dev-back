6de7f1f3c6b48d0a4c612d4cc5ebb5b1aed42726
Ashir Azeem <ashir.azeem@ratechnologies.biz>
2022-02-20 21:40:11 +0500

    Added Bill Model

---
+++ Domain/Entities/BillLines.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class BillLines : BaseEntity<int>
+    {
+        public int? ItemId { get; private set; }
+        [ForeignKey("ItemId")]
+        public Product Item { get; private set; }
+        [MaxLength(500)]
+        public string Description { get; private set; }
+        public int Quantity { get; private set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Cost { get; private set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Tax { get; private set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal SubTotal { get; private set; }
+        public Guid AccountId { get; private set; }
+        [ForeignKey("AccountId")]
+        public Level4 Account { get; private set; }
+        public int LocationId { get; private set; }
+        [ForeignKey("LocationId")]
+        public Location Location { get; private set; }
+        public int MasterId { get; private set; }
+
+        [ForeignKey("MasterId ")]
+        public BillMaster BillMaster { get; private set; }
+
+        protected BillLines()
+        {
+
+        }
+    }
+}
---
+++ Domain/Entities/BillMaster.cs
@@ -0,0 +1,50 @@
+﻿using Domain.Base;
+using Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class BillMaster : BaseEntity<int>
+    {
+        public int VendorId { get; private set; }
+        [ForeignKey("VendorId")]
+        public BusinessPartner Vendor { get; private set; }
+        [MaxLength(50)]
+        public string DocNo { get; private set; }
+        [MaxLength(20)]
+        public string VendorBillRef { get; private set; }
+        public DateTime BillDate { get; private set; }
+        public DateTime DueDate { get; private set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TotalBeforeTax { get; private set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TotalTax { get; private set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TotalAmount { get; private set; }
+        [MaxLength(20)]
+        public string Contact { get; private set; }
+        public DocumentStatus Status { get; private set; }
+        public virtual List<BillLines> BillLines { get; private set; }
+
+        protected BillMaster()
+        {
+
+        }
+        public void setStatus(DocumentStatus status)
+        {
+            Status = status;
+        }
+
+        public void CreateDocNo()
+        {
+            //Creating doc no..
+            DocNo = "BILL-" + String.Format("{0:000}", Id);
+        }
+    }
+}
