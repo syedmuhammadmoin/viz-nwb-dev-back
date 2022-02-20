6fa62cfb542a6feefa8bed464b038c64ec847963
Ashir Azeem <ashir.azeem@ratechnologies.biz>
2022-02-21 00:42:21 +0500

    Added CreditNote Controller

---
+++ VizalysDDD/Controllers/CreditNoteController.cs
@@ -0,0 +1,51 @@
+﻿using Application.Contracts.DTOs;
+using Application.Contracts.Filters;
+using Application.Contracts.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vizalys.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreditNoteController : ControllerBase
+    {
+        private readonly ICreditNoteService _creditNoteService;
+
+        public CreditNoteController(ICreditNoteService creditNoteService)
+        {
+            _creditNoteService = creditNoteService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CreditNoteDto>> CreateAsync(CreateCreditNoteDto entity)
+        {
+            var CreditNote = await _creditNoteService.CreateAsync(entity);
+            return Ok(CreditNote);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<CreditNoteDto>>> GetAllAsync([FromQuery] PaginationFilter filter)
+        {
+            var invs = await _creditNoteService.GetAllAsync(filter);
+            return Ok(invs);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<CreditNoteDto>> GetByIdAsync(int id)
+        {
+            var result = await _creditNoteService.GetByIdAsync(id);
+            return Ok(result); // Status Code : 200
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<CreditNoteDto>> UpdateAsync(int id, CreateCreditNoteDto entity)
+        {
+            if (id != entity.Id)
+                return BadRequest("ID mismatch");
+
+            var result = await _creditNoteService.UpdateAsync(entity);
+            return Ok(result); // Status Code : 200
+        }
+    }
+}
