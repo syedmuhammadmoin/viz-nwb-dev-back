fdebedccc536fbcd674adb5f2cd9c6fc6fd9f985
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-02-21 19:28:47 +0500

    Added Transaction into JVSpecs

--- Infrastructure/Specifications/JournalEntrySpecs.cs
+++ Infrastructure/Specifications/JournalEntrySpecs.cs
@@ -14,6 +14,7 @@ namespace Infrastructure.Specifications
         {
             var validFilter = new PaginationFilter(filter.PageStart, filter.PageEnd);
             ApplyPaging(validFilter.PageStart, validFilter.PageEnd - validFilter.PageStart);
+            AddInclude(i => i.Transactions);
             AddInclude("JournalEntryLines.BusinessPartner");
             AddInclude("JournalEntryLines.Account");
             AddInclude("JournalEntryLines.Location");
@@ -21,6 +22,7 @@ namespace Infrastructure.Specifications
 
         public JournalEntrySpecs()
         {
+            AddInclude(i => i.Transactions);
             AddInclude("JournalEntryLines.BusinessPartner");
             AddInclude("JournalEntryLines.Account");
             AddInclude("JournalEntryLines.Location");
