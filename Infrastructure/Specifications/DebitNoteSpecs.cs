c76ffa25ca32d9efa66a5bdded4a4d3775f15a11
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-02-21 11:46:12 +0500

    Added DebitNote Specs

---
+++ Infrastructure/Specifications/DebitNoteSpecs.cs
@@ -0,0 +1,39 @@
+﻿using Application.Contracts.Filters;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Specifications
+{
+    public class DebitNoteSpecs : BaseSpecification<DebitNoteMaster>
+    {
+        public DebitNoteSpecs(PaginationFilter filter)
+        {
+            var validFilter = new PaginationFilter(filter.PageStart, filter.PageEnd);
+            ApplyPaging(validFilter.PageStart, validFilter.PageEnd - validFilter.PageStart);
+            AddInclude(i => i.Vendor);
+            AddInclude("DebitNoteLines.Account");
+            AddInclude("DebitNoteLines.Location");
+            AddInclude("DebitNoteLines.Item");
+        }
+
+        public DebitNoteSpecs(bool forEdit)
+        {
+            if (forEdit)
+            {
+                AddInclude(i => i.DebitNoteLines);
+            }
+            else
+            {
+                AddInclude(i => i.Vendor);
+                AddInclude("DebitNoteLines.Account");
+                AddInclude("DebitNoteLines.Location");
+                AddInclude("DebitNoteLines.Item");
+            }
+
+        }
+    }
+}
