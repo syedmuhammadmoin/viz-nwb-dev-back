573fd0c30a175cf9f934efe515d7590c110f4951
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-02-24 18:28:13 +0500

    Added CashAccountService

---
+++ Application.Contracts/Interfaces/ICashAccountService.cs
@@ -0,0 +1,15 @@
+﻿using Application.Contracts.DTOs;
+using Application.Contracts.Filters;
+using Application.Contracts.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Interfaces
+{
+    public interface ICashAccountService : ICrudService<CreateCashAccountDto, CashAccountDto, int, PaginationFilter>
+    {
+    }
+}
---
+++ Application/Services/CashAccountService.cs
@@ -0,0 +1,140 @@
+﻿using Application.Contracts.DTOs;
+using Application.Contracts.Filters;
+using Application.Contracts.Interfaces;
+using Application.Contracts.Response;
+using AutoMapper;
+using Domain.Constants;
+using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class CashAccountService : ICashAccountService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public CashAccountService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Response<CashAccountDto>> CreateAsync(CreateCashAccountDto entity)
+        {
+            _unitOfWork.CreateTransaction();
+            try
+            {
+                var ChAccount = new Level4(
+                    entity.CashAccountName,
+                    new Guid("12200000-5566-7788-99AA-BBCCDDEEFF00"),
+                    new Guid("10000000-5566-7788-99AA-BBCCDDEEFF00"));
+
+                await _unitOfWork.Level4.Add(ChAccount);
+
+                var transaction = new Transactions("1", DocType.CashAccount);
+                await _unitOfWork.Transaction.Add(transaction);
+                await _unitOfWork.SaveAsync();
+
+                //Inserting into cashAccount table
+                var cashAccount = _mapper.Map<CashAccount>(entity);
+
+                cashAccount.setChAccountId(ChAccount.Id);
+                cashAccount.setTransactionId(transaction.Id);
+
+                await _unitOfWork.CashAccount.Add(cashAccount);
+                await _unitOfWork.SaveAsync();
+
+                cashAccount.createDocNo();
+                transaction.updateDocNo(cashAccount.DocNo);
+                await _unitOfWork.SaveAsync();
+
+                //Adding CashAccount to Ledger
+                await AddToLedger(cashAccount);
+
+                //Commiting the transaction 
+                _unitOfWork.Commit();
+
+                //returning response
+                return new Response<CashAccountDto>(_mapper.Map<CashAccountDto>(cashAccount), "Created successfully");
+            }
+            catch (Exception ex)
+            {
+                _unitOfWork.Rollback();
+                return new Response<CashAccountDto>(ex.Message);
+            }
+        }
+
+        public async Task<PaginationResponse<List<CashAccountDto>>> GetAllAsync(PaginationFilter filter)
+        {
+            var specification = new CashAccountSpecs(filter);
+            var CashAccount = await _unitOfWork.CashAccount.GetAll(specification);
+
+            if (CashAccount.Count() == 0)
+                return new PaginationResponse<List<CashAccountDto>>("List is empty");
+
+            var totalRecords = await _unitOfWork.CashAccount.TotalRecord();
+
+            return new PaginationResponse<List<CashAccountDto>>(_mapper.Map<List<CashAccountDto>>(CashAccount), filter.PageStart, filter.PageEnd, totalRecords, "Returing list");
+
+        }
+
+        public async Task<Response<CashAccountDto>> GetByIdAsync(int id)
+        {
+            var CashAccount = await _unitOfWork.CashAccount.GetById(id);
+            if (CashAccount == null)
+                return new Response<CashAccountDto>("Not found");
+
+            return new Response<CashAccountDto>(_mapper.Map<CashAccountDto>(CashAccount), "Returning value");
+        }
+
+        public async Task<Response<CashAccountDto>> UpdateAsync(CreateCashAccountDto entity)
+        {
+            var cashAccount = await _unitOfWork.CashAccount.GetById((int)entity.Id);
+
+            if (cashAccount == null)
+                return new Response<CashAccountDto>("Not found");
+
+            cashAccount.updateCashAccount(entity.CashAccountName, entity.Handler);
+            await _unitOfWork.SaveAsync();
+            return new Response<CashAccountDto>(_mapper.Map<CashAccountDto>(cashAccount), "Updated successfully");
+        }
+
+        public Task<Response<int>> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task AddToLedger(CashAccount cashAccount)
+        {
+            var addBalanceInCashAccount = new RecordLedger(
+                cashAccount.TransactionId,
+                cashAccount.ChAccountId,
+                null,
+                null,
+                "Opening Balance",
+                'D',
+                cashAccount.OpeningBalance);
+
+            await _unitOfWork.Ledger.Add(addBalanceInCashAccount);
+
+            var addBalanceInOpeningBalanceEquity = new RecordLedger(
+               cashAccount.TransactionId,
+               new Guid("12210000-5566-7788-99AA-BBCCDDEEFF04"),
+               null,
+               null,
+               cashAccount.DocNo,
+               'C',
+               cashAccount.OpeningBalance);
+
+            await _unitOfWork.Ledger.Add(addBalanceInOpeningBalanceEquity);
+            await _unitOfWork.SaveAsync();
+        }
+    }
+}
