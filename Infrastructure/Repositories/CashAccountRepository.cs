1d68db8b15ae9ccd23312712fb3d1b39ebeaa56c
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-02-24 18:29:46 +0500

    Added CashAccountRepo

---
+++ Infrastructure/Repositories/CashAccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class CashAccountRepository : GenericRepository<CashAccount, int>, ICashAccountRepository
+    {
+        public CashAccountRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
