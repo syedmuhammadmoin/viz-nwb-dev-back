3c681cae2ef11bf67d1b46ad2db2a05fb28019ba
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-03-03 19:21:39 +0500

    Added BankStmtDto

---
+++ Application.Contracts/DTOs/BankStmt/BankStmtLinesDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class BankStmtLinesDto
+    {
+        public int Id { get; set; }
+        public int Reference { get; set; }
+        public DateTime StmtDate { get; set; }
+        public string Label { get; set; }
+        public decimal Debit { get; set; }
+        public decimal Credit { get; set; }
+        public int MasterId { get; set; }
+
+    }
+}
