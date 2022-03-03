66c54453fff2a44ea435f91294c753d5bdf30a91
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-03-03 19:22:32 +0500

    Added BankStmtLine and BankStmtMaster Model

---
+++ Domain/Entities/BankStmtLines.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Base;
+using Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class BankStmtLines : BaseEntity<int>
+    {
+        public int Reference { get; private set; }
+        public DateTime StmtDate { get; private set; }
+        [MaxLength(50)]
+        public string Label { get; private set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Debit { get; private set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Credit { get; private set; }
+        public int MasterId { get; private set; }
+        [ForeignKey("MasterId")]
+        public BankStmtMaster BankStmtMaster { get; private set; }
+
+        protected BankStmtLines()
+        {
+
+        }
+
+        public BankStmtLines(int reference, DateTime stmtDate, string label, decimal debit, decimal credit, ReconStatus bankReconStatus)
+        {
+            Reference = reference;
+            StmtDate = stmtDate;
+            Label = label;
+            Debit = debit;
+            Credit = credit;
+        }
+    }
+}
---
+++ Domain/Entities/BankStmtMaster.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Base;
+using Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class BankStmtMaster : BaseEntity<int>
+    {
+        public int BankAccountId { get; private set; }
+        [ForeignKey("BankAccountId")]
+        public BankAccount BankAccount { get; private set; }
+        [MaxLength(500)]
+        public string Description { get; private set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal OpeningBalance { get; private set; }
+        public ReconStatus BankReconStatus { get; private set; }
+        public virtual List<BankStmtLines> BankStmtLines { get; private set; }
+
+        protected BankStmtMaster()
+        {
+
+        }
+
+        public void setStatus(ReconStatus bankReconstatus)
+        {
+            BankReconStatus = bankReconstatus;
+        }
+
+
+        public BankStmtMaster(int bankAccountId, string description, decimal openingBalance, ReconStatus bankReconStatus, List<BankStmtLines> bankStmtLines)
+        {
+            BankAccountId = bankAccountId;
+            Description = description;
+            OpeningBalance = openingBalance;
+            BankReconStatus = bankReconStatus;
+            BankStmtLines = bankStmtLines;
+        }
+    }
+}
