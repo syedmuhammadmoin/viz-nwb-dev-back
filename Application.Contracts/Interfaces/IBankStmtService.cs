cf22b95295117d819b8f59f62a4a74e9123d7880
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-03-04 18:59:37 +0500

    Updated CreateBankStmt Method

--- Application.Contracts/Interfaces/IBankStmtService.cs
+++ Application.Contracts/Interfaces/IBankStmtService.cs
@@ -1,5 +1,7 @@
 ﻿using Application.Contracts.DTOs;
 using Application.Contracts.Filters;
+using Application.Contracts.Response;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +12,6 @@ namespace Application.Contracts.Interfaces
 {
     public interface IBankStmtService : ICrudService<CreateBankStmtDto, BankStmtDto, int, PaginationFilter>
     {
+        Task<Response<BankStmtDto>> CreateAsync(CreateBankStmtDto entity, IFormFile file);
     }
 }
