822e3e6ad8362b0c71669c35154ff6ab52ca2565
Waleed Tariq <dev.ratech@outlook.com>
2022-03-13 14:34:43 +0500

    add BankStmtLines Repo

---
+++ Domain/Interfaces/IBankStmtLinesRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IBankStmtLinesRepository
+    {
+        Task<IEnumerable<BankStmtLines>> GetAll(ISpecification<BankStmtLines> specification = null);
+        Task<BankStmtLines> GetById(int id, ISpecification<BankStmtLines> specification = null);
+    }
+}
