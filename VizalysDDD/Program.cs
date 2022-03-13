a91cceab4b188ccb11019be727d84c54016529b6
Waleed Tariq <dev.ratech@outlook.com>
2022-03-13 14:40:10 +0500

    add cors origin

--- VizalysDDD/Program.cs
+++ VizalysDDD/Program.cs
@@ -48,15 +48,30 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200")
+                                        .AllowAnyHeader()
+                                        .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 UserSeeding.Initialize(app.Services);
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseCors();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
