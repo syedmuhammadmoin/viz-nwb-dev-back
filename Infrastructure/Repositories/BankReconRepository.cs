db79ff674e73e44dd12469851bdcba88333ea76f
Waleed Tariq <dev.ratech@outlook.com>
2022-03-13 14:34:16 +0500

    add bankRecon Repo

---
+++ Infrastructure/Repositories/BankReconRepository.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Context;
+using Infrastructure.Specifications;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class BankReconRepository : GenericRepository<BankReconciliation, int>, IBankReconRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public BankReconRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<decimal> GetReconciledAmountById(int id, bool isPaymetId)
+        {
+            if (isPaymetId)
+            {
+                return await _context.BankReconciliations
+                                    .Where(p => p.PaymentId == id)
+                                    .SumAsync(p => p.Amount);
+            }
+            else
+            {
+                return await _context.BankReconciliations
+                                    .Where(p => p.BankStmtId == id)
+                                    .SumAsync(p => p.Amount);
+            }
+        }
+    }
+}
