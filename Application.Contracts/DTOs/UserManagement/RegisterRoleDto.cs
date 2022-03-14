fbda5f507445ed64192c2721e8416b7753073d2c
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-03-14 20:23:19 +0500

    Added RegisterDto

---
+++ Application.Contracts/DTOs/UserManagement/RegisterRoleDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class RegisterRoleDto
+    {
+        [Required]
+        public string RoleName { get; set; }
+        public IList<RegisterRoleClaimsDto> RoleClaims { get; set; }
+
+    }
+    public class RegisterRoleClaimsDto
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+        public bool Selected { get; set; }
+    }
+}
