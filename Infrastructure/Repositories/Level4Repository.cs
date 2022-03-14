83f03f77f610b63ae3c0671e96b0419d3dfeb6f4
Waleed Tariq <dev.ratech@outlook.com>
2022-03-14 17:59:51 +0500

    add coa service and seeding

--- Infrastructure/Repositories/Level4Repository.cs
+++ Infrastructure/Repositories/Level4Repository.cs
@@ -6,13 +6,46 @@ using Infrastructure.Context;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories
 {
     public class Level4Repository : GenericRepository<Level4, Guid>, ILevel4Repository
     {
+        private readonly ApplicationDbContext _context;
+
         public Level4Repository(ApplicationDbContext context) : base(context)
         {
+            _context = context;
+        }
+
+        public async Task<List<Level1>> GetCOA()
+        {
+            return await _context.Level1
+                .Include(c => c.Level2)
+                .ThenInclude(x => x.Level3)
+                .ThenInclude(x => x.Level4)
+                .Select(x => new Level1
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Level2 = x.Level2.Select(y => new Level2
+                    {
+                        Id = y.Id,
+                        Name = y.Name,
+                        Level3 = y.Level3.Select(z => new Level3
+                        {
+                            Id = z.Id,
+                            Name = z.Name,
+                            Level4 = z.Level4.Select(a => new Level4
+                            {
+                                Id = a.Id,
+                                Name = a.Name
+                            })
+                        })
+                    })
+                })
+                .ToListAsync();
         }
     }
 }
---
+++ VizalysDDD/Controllers/COAController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Contracts.DTOs;
+using Application.Contracts.Interfaces;
+using Application.Contracts.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vizalys.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class COAController : ControllerBase
+    {
+        private readonly ICOAService _coaService;
+
+        public COAController(ICOAService coaService)
+        {
+            _coaService = coaService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Response<List<Level1Dto>>>> GetChartOfAccounts()
+        {
+            return Ok(await _coaService.GetCOA()); // Status Code : 200
+        }
+
+    }
+}
