36ca0f9c0ec5c3cc5797092ec05e6ca0000e5367
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-03-21 19:45:47 +0500

    Added WorkFlow Repo

---
+++ Domain/Interfaces/IWorkFlowRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IWorkFlowRepository : IGenericRepository<WorkFlowMaster, int>
+    {
+        Task<WorkFlowMaster> Find(ISpecification<WorkFlowMaster> specification);
+    }
+
+    public interface IWorkFlowStatusRepository : IGenericRepository<WorkFlowStatus, int>
+    {
+
+    }
+}
---
+++ Infrastructure/Repositories/WorkFlowRepository.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Context;
+using Infrastructure.Specifications;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class WorkFlowRepository : GenericRepository<WorkFlowMaster, int>, IWorkFlowRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public WorkFlowRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<WorkFlowMaster> Find(ISpecification<WorkFlowMaster> specification)
+        {
+            return await SpecificationEvaluator<WorkFlowMaster, int>.GetQuery(_context.WorkFlowMaster
+                                    .AsQueryable(), specification)
+                                    .FirstOrDefaultAsync();
+        }
+    }
+
+    public class WorkFlowStatusRepository : GenericRepository<WorkFlowStatus, int>, IWorkFlowStatusRepository
+    {
+        public WorkFlowStatusRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
