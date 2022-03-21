be7250834baea6539662b7bc1a74d6ec96d3f086
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-03-21 19:46:31 +0500

    Added WorkFlowSpecs

---
+++ Infrastructure/Specifications/WorkFlowSpecs.cs
@@ -0,0 +1,36 @@
+﻿using Application.Contracts.Filters;
+using Domain.Constants;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Specifications
+{
+    public class WorkFlowSpecs : BaseSpecification<WorkFlowMaster>
+    {
+        public WorkFlowSpecs(PaginationFilter filter)
+        {
+            var validFilter = new PaginationFilter(filter.PageStart, filter.PageEnd);
+            ApplyPaging(validFilter.PageStart, validFilter.PageEnd - validFilter.PageStart);
+            AddInclude("WorkflowTransitions.WorkFlowStatus");
+        }
+        public WorkFlowSpecs(bool forEdit)
+        {
+            if (forEdit)
+            {
+                AddInclude(i => i.WorkflowTransitions);
+            }
+            else
+            {
+                AddInclude("WorkflowTransitions.WorkFlowStatus");
+            }
+        }
+
+        public WorkFlowSpecs(DocType docType) : base(e => (e.DocType == docType) && (e.IsActive == true))
+        {
+        }
+    }
+}
---
+++ Infrastructure/Specifications/WorkflowStatusSpecs.cs
@@ -0,0 +1,19 @@
+﻿using Application.Contracts.Filters;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Specifications
+{
+    public class WorkflowStatusSpecs : BaseSpecification<WorkFlowStatus>
+    {
+        public WorkflowStatusSpecs(PaginationFilter filter)
+        {
+            var validFilter = new PaginationFilter(filter.PageStart, filter.PageEnd);
+            ApplyPaging(validFilter.PageStart, validFilter.PageEnd - validFilter.PageStart);
+        }
+    }
+}
