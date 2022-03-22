b8b7b0b1bd615583d8c78c756f247ecc294154ec
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-03-22 15:31:54 +0500

    Added Budget Model

---
+++ Domain/Entities/BudgetLines.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class BudgetLines : BaseEntity<int>
+    {
+        public Guid AccountId { get; private set; }
+        [ForeignKey("AccountId")]
+        public Level4 Account { get; private set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; private set; }
+        public int MasterId { get; private set; }
+        [ForeignKey("MasterId")]
+        [JsonIgnore]
+        public BudgetMaster BudgetMaster { get; private set; }
+    }
+}
