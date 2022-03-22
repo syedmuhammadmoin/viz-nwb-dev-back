decd659f235a54534da101adedf2448b00712418
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-03-22 15:31:42 +0500

    Added Budget DTO

---
+++ Application.Contracts/DTOs/Budget/CreateBudgetDto.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class CreateBudgetDto
+    {
+        public int? Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string BudgetName { get; set; }
+        [Required]
+        public DateTime From { get; set; }
+        [Required]
+        public DateTime To { get; set; }
+        public virtual List<CreateBudgetLinesDto> BudgetLines { get; set; }
+    }
+}
