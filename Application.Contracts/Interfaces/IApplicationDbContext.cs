0b90c935f7e8db6e997950204a22884b846308aa
Waleed Tariq <dev.ratech@outlook.com>
2022-03-25 03:00:40 +0500

    add seeding and change table order

--- Application.Contracts/Interfaces/IApplicationDbContext.cs
+++ Application.Contracts/Interfaces/IApplicationDbContext.cs
@@ -14,9 +14,21 @@ namespace Application.Interfaces
         public DbSet<Department> Departments { get; set; }
         public DbSet<Location> Locations { get; set; }
         public DbSet<Warehouse> Warehouses { get; set; }
-        public DbSet<Category> Categories { get; set; }
+        public DbSet<Level1> Level1 { get; set; }
+        public DbSet<Level2> Level2 { get; set; }
+        public DbSet<Level3> Level3 { get; set; }
         public DbSet<Level4> Level4 { get; set; }
+        public DbSet<BusinessPartner> BusinessPartners { get; set; }
+        public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<CashAccount> CashAccounts { get; set; }
+        public DbSet<BankAccount> BankAccounts { get; set; }
+        public DbSet<Transactions> Transactions { get; set; }
+        public DbSet<RecordLedger> RecordLedger { get; set; }
+        public DbSet<BankStmtMaster> BankStmtMaster { get; set; }
+        public DbSet<BankStmtLines> BankStmtLines { get; set; }
+        public DbSet<WorkFlowStatus> WorkFlowStatus { get; set; }
+
         public DbSet<JournalEntryMaster> JournalEntryMaster { get; set; }
         public DbSet<JournalEntryLines> JournalEntryLines { get; set; }
         public DbSet<InvoiceMaster> InvoiceMaster { get; set; }
@@ -28,18 +40,10 @@ namespace Application.Interfaces
         public DbSet<DebitNoteMaster> DebitNoteMaster { get; set; }
         public DbSet<DebitNoteLines> DebitNoteLines { get; set; }
         public DbSet<Payment> Payments { get; set; }
-        public DbSet<Transactions> Transactions { get; set; }
-        public DbSet<RecordLedger> RecordLedger { get; set; }
-        public DbSet<CashAccount> CashAccounts { get; set; }
-        public DbSet<BankAccount> BankAccounts { get; set; }
-        public DbSet<BankStmtMaster> BankStmtMaster { get; set; }
-        public DbSet<BankStmtLines> BankStmtLines { get; set; }
         public DbSet<BankReconciliation> BankReconciliations { get; set; }
-        public DbSet<WorkFlowStatus> WorkFlowStatus { get; set; }
         public DbSet<WorkFlowMaster> WorkFlowMaster { get; set; }
         public DbSet<WorkFlowTransition> WorkFlowTransitions { get; set; }
         public DbSet<TransactionReconcile> TransactionReconciles { get; set; }
-
 
         Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
