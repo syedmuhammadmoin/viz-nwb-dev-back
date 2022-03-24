a14a2d37dd94cd6b7e53cb6a1f805b88bc42e62a
Waleed Tariq <dev.ratech@outlook.com>
2022-03-25 02:58:52 +0500

    remove get specific method

--- Infrastructure/Repositories/BankReconRepository.cs
+++ Infrastructure/Repositories/BankReconRepository.cs
@@ -13,26 +13,8 @@ namespace Infrastructure.Repositories
 {
     public class BankReconRepository : GenericRepository<BankReconciliation, int>, IBankReconRepository
     {
-        private readonly ApplicationDbContext _context;
         public BankReconRepository(ApplicationDbContext context) : base(context)
         {
-            _context = context;
-        }
-
-        public async Task<decimal> GetReconciledAmountById(int id, bool isPaymetId)
-        {
-            if (isPaymetId)
-            {
-                return await _context.BankReconciliations
-                                    .Where(p => p.PaymentId == id)
-                                    .SumAsync(p => p.Amount);
-            }
-            else
-            {
-                return await _context.BankReconciliations
-                                    .Where(p => p.BankStmtId == id)
-                                    .SumAsync(p => p.Amount);
-            }
         }
     }
 }
