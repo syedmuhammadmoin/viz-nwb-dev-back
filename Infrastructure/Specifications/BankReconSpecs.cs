6d289a173d7e327b0eb6a86e9a85b51e6aa85e7c
Waleed Tariq <dev.ratech@outlook.com>
2022-03-25 02:59:29 +0500

    add specs for find

---
+++ Infrastructure/Specifications/BankReconSpecs.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Specifications
+{
+    public class BankReconSpecs : BaseSpecification<BankReconciliation>
+    {
+        public BankReconSpecs(int id, bool isPaymentId)
+            : base(isPaymentId ? p => p.PaymentId == id
+            : p => p.BankStmtId == id)
+        { }
+    }
+}
