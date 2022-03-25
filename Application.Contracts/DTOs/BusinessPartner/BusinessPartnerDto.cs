40094496e964ce41f582c5b9513eb6ac23e705c4
Waleed Tariq <dev.ratech@outlook.com>
2022-03-25 16:34:15 +0500

    updated Dto with corrections

--- Application.Contracts/DTOs/BusinessPartner/BusinessPartnerDto.cs
+++ Application.Contracts/DTOs/BusinessPartner/BusinessPartnerDto.cs
@@ -11,13 +11,9 @@ namespace Application.Contracts.DTOs
     {
         public int Id { get; set; }
         public BusinessPartnerType BusinessPartnerType { get; set; }
-        public string Entity { get; set; }
         public string Name { get; set; }
         public string CNIC { get; set; }
         public string Address { get; set; }
-        public string Country { get; set; }
-        public string State { get; set; }
-        public string City { get; set; }
         public string Phone { get; set; }
         public string Mobile { get; set; }
         public string Email { get; set; }
