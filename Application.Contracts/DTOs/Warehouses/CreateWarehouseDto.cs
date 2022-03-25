dbe745fce58eaca29ac5fd7fbd0bb14f746d47ca
Waleed Tariq <dev.ratech@outlook.com>
2022-03-25 16:44:40 +0500

    added campus

--- Application.Contracts/DTOs/Warehouses/CreateWarehouseDto.cs
+++ Application.Contracts/DTOs/Warehouses/CreateWarehouseDto.cs
@@ -9,20 +9,9 @@ namespace Application.Contracts.DTOs
         [MaxLength(100)]
         public string Name { get; set; }
         [Required]
-        [MaxLength(50)]
-        public string Country { get; set; }
-        [Required]
-        [MaxLength(50)]
-        public string State { get; set; }
-        [Required]
-        [MaxLength(50)]
-        public string City { get; set; }
-        [MaxLength(200)]
-        public string Address { get; set; }
-
         [MaxLength(100)]
         public string Manager { get; set; }
         [Required]
-        public int DepartmentId { get; set; }
+        public int CampusId { get; set; }
     }
 }
--- Application.Contracts/DTOs/Warehouses/WarehouseDto.cs
+++ Application.Contracts/DTOs/Warehouses/WarehouseDto.cs
@@ -5,12 +5,7 @@ namespace Application.Contracts.DTOs
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Country { get; set; }
-        public string State { get; set; }
-        public string City { get; set; }
-        public string Address { get; set; }
         public string Manager { get; set; }
-        public int DepartmentId { get; set; }
-        public string DepartmentName { get; set; }
+        public string Campus { get; set; }
     }
 }
