8bd85abda56a5b911921c517e8ca56b6cb2f4737
Waleed Tariq <dev.ratech@outlook.com>
2022-03-25 16:44:59 +0500

    updated specs

--- Infrastructure/Specifications/WarehouseSpecs.cs
+++ Infrastructure/Specifications/WarehouseSpecs.cs
@@ -14,11 +14,6 @@ namespace Infrastructure.Specifications
         {
             var validFilter = new PaginationFilter(filter.PageStart, filter.PageEnd);
             ApplyPaging(validFilter.PageStart, validFilter.PageEnd - validFilter.PageStart);
-            AddInclude(i => i.Department);
-        }
-        public WarehouseSpecs()
-        {
-            AddInclude(i => i.Department);
         }
     }
 }
