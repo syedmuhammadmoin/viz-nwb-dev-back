38cd227f38ab1223ccd1fb8f0d9fdd08ea4d35e0
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-03-26 14:49:57 +0500

    Added WorkFlowStatus Service

---
+++ Application.Contracts/Interfaces/IWorkFlowStatusService.cs
@@ -0,0 +1,17 @@
+﻿using Application.Contracts.DTOs;
+using Application.Contracts.Filters;
+using Application.Contracts.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Interfaces
+{
+
+    public interface IWorkFlowStatusService : ICrudService<CreateWorkFlowStatusDto, WorkFlowStatusDto, int, PaginationFilter>
+    {
+        Task<Response<List<WorkFlowStatusDto>>> GetStatusDropDown();
+    }
+}
