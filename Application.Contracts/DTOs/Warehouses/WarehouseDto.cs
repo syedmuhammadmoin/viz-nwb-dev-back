ee70159e2c53ba96ba9fb785bf709fb5c209807f
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-03-26 14:52:22 +0500

    UpdatedDto

--- Application.Contracts/DTOs/Warehouses/WarehouseDto.cs
+++ Application.Contracts/DTOs/Warehouses/WarehouseDto.cs
@@ -6,6 +6,7 @@ namespace Application.Contracts.DTOs
         public int Id { get; set; }
         public string Name { get; set; }
         public string Manager { get; set; }
-        public string Campus { get; set; }
+        public string CampusName { get; set; }
+        public int CampusId{ get; set; }
     }
 }
