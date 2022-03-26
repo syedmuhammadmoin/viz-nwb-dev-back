8dcfb183be4399c6e1cdc4c75d66721fb483ce9a
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-03-26 17:03:52 +0500

    Added WorkFlow Controller

--- VizalysDDD/Controllers/WorkFlowController.cs
+++ VizalysDDD/Controllers/WorkFlowController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Application.Contracts.DTOs;
+using Application.Contracts.Filters;
+using Application.Contracts.Helper;
+using Application.Contracts.Interfaces;
+using Application.Contracts.Response;
+using Domain.Constants;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Vizalys.Api.Controllers
@@ -7,5 +13,65 @@ namespace Vizalys.Api.Controllers
     [ApiController]
     public class WorkFlowController : ControllerBase
     {
+        private readonly IWorkFlowService _workFlowService;
+
+        public WorkFlowController(IWorkFlowService workFlowService)
+        {
+            _workFlowService = workFlowService;
+        }
+
+        [ClaimRequirement("Permission", new string[] { Permissions.WorkflowClaims.Create, Permissions.WorkflowClaims.View, Permissions.WorkflowClaims.Delete, Permissions.WorkflowClaims.Edit })]
+        [HttpGet]
+        public async Task<ActionResult<PaginationResponse<List<WorkFlowDto>>>> GetAllAsync([FromQuery] PaginationFilter filter)
+        {
+            var campuses = await _workFlowService.GetAllAsync(filter);
+            if (campuses.IsSuccess)
+                return Ok(campuses); // Status Code : 200
+
+            return BadRequest(campuses); // Status code : 400
+        }
+
+        [ClaimRequirement("Permission", new string[] { Permissions.WorkflowClaims.Create })]
+        [HttpPost]
+        public async Task<ActionResult<Response<WorkFlowDto>>> CreateAsync(CreateWorkFlowDto entity)
+        {
+            var campus = await _workFlowService.CreateAsync(entity);
+            if (campus.IsSuccess)
+                return Ok(campus); // Status Code : 200
+
+            return BadRequest(campus); // Status code : 400
+        }
+
+        [ClaimRequirement("Permission", new string[] { Permissions.WorkflowClaims.View, Permissions.WorkflowClaims.Delete, Permissions.WorkflowClaims.Edit })]
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Response<WorkFlowDto>>> GetByIdAsync(int id)
+        {
+            var result = await _workFlowService.GetByIdAsync(id);
+            if (result.IsSuccess)
+                return Ok(result); // Status Code : 200
+
+            return BadRequest(result); // Status code : 400
+        }
+
+        [ClaimRequirement("Permission", new string[] { Permissions.WorkflowClaims.Edit })]
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<Response<WorkFlowDto>>> UpdateAsync(int id, CreateWorkFlowDto entity)
+        {
+            if (id != entity.Id)
+                return BadRequest("ID mismatch");
+
+            var result = await _workFlowService.UpdateAsync(entity);
+            if (result.IsSuccess)
+                return Ok(result); // Status Code : 200
+
+            return BadRequest(result); // Status code : 400
+        }
+
+        //[HttpGet("Dropdown")]
+        //public async Task<ActionResult<Response<List<WorkFlowDto>>>> GetStatusDropDown()
+        //{
+        //    return Ok(await _workFlowService.GetStatusDropDown()); // Status Code : 200
+        //}
+
     }
 }
