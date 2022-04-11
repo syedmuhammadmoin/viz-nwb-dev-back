aea610b9d7fb390a4bbb1359b2a24aa7dfe4b81f
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-04-11 11:47:53 +0500

    added  Designation & Department controller

---
+++ VizalysDDD/Controllers/DepartmentController.cs
@@ -0,0 +1,78 @@
+﻿using Application.Contracts.DTOs;
+using Application.Contracts.Filters;
+using Application.Contracts.Helper;
+using Application.Contracts.Interfaces;
+using Application.Contracts.Response;
+using Domain.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vizalys.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IDepartmentService _departmentService;
+
+        public DepartmentController(IDepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+        [ClaimRequirement("Permission", new string[] { Permissions.DepartmentClaims.Create, Permissions.DepartmentClaims.View, Permissions.DepartmentClaims.Delete, Permissions.DepartmentClaims.Edit })]
+        [HttpGet]
+        public async Task<ActionResult<PaginationResponse<List<DepartmentDto>>>> GetAllAsync([FromQuery] PaginationFilter filter)
+        {
+            var departments = await _departmentService.GetAllAsync(filter);
+            if (departments.IsSuccess)
+                return Ok(departments); // Status Code : 200
+
+            return BadRequest(departments); // Status code : 400
+        }
+
+        [ClaimRequirement("Permission", new string[] { Permissions.DepartmentClaims.Create })]
+        [HttpPost]
+        public async Task<ActionResult<Response<DepartmentDto>>> CreateAsync(DepartmentDto entity)
+        {
+            var department = await _departmentService.CreateAsync(entity);
+            if (department.IsSuccess)
+                return Ok(department); // Status Code : 200
+
+            return BadRequest(department); // Status code : 400
+        }
+
+        [ClaimRequirement("Permission", new string[] { Permissions.DepartmentClaims.View, Permissions.DepartmentClaims.Delete, Permissions.DepartmentClaims.Edit })]
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Response<DepartmentDto>>> GetByIdAsync(int id)
+        {
+            var result = await _departmentService.GetByIdAsync(id);
+            if (result.IsSuccess)
+                return Ok(result); // Status Code : 200
+
+            return BadRequest(result); // Status code : 400
+        }
+
+        [ClaimRequirement("Permission", new string[] { Permissions.DepartmentClaims.Edit })]
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<Response<DepartmentDto>>> UpdateAsync(int id, DepartmentDto entity)
+        {
+            if (id != entity.Id)
+                return BadRequest("ID mismatch");
+
+            var result = await _departmentService.UpdateAsync(entity);
+            if (result.IsSuccess)
+                return Ok(result); // Status Code : 200
+
+            return BadRequest(result); // Status code : 400
+        }
+
+        [HttpGet("Dropdown")]
+        public async Task<ActionResult<Response<List<DepartmentDto>>>> GetDepartmentDropDown()
+        {
+            return Ok(await _departmentService.GetDepartmentDropDown()); // Status Code : 200
+        }
+    }
+}
---
+++ VizalysDDD/Controllers/DesignationController.cs
@@ -0,0 +1,77 @@
+﻿using Application.Contracts.DTOs;
+using Application.Contracts.Filters;
+using Application.Contracts.Helper;
+using Application.Contracts.Interfaces;
+using Application.Contracts.Response;
+using Domain.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vizalys.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class DesignationController : ControllerBase
+    {
+        private readonly IDesignationService _departmentService;
+
+        public DesignationController(IDesignationService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+        [ClaimRequirement("Permission", new string[] { Permissions.DesignationClaims.Create, Permissions.DesignationClaims.View, Permissions.DesignationClaims.Delete, Permissions.DesignationClaims.Edit })]
+        [HttpGet]
+        public async Task<ActionResult<PaginationResponse<List<DesignationDto>>>> GetAllAsync([FromQuery] PaginationFilter filter)
+        {
+            var departments = await _departmentService.GetAllAsync(filter);
+            if (departments.IsSuccess)
+                return Ok(departments); // Status Code : 200
+
+            return BadRequest(departments); // Status code : 400
+        }
+
+        [ClaimRequirement("Permission", new string[] { Permissions.DesignationClaims.Create })]
+        [HttpPost]
+        public async Task<ActionResult<Response<DesignationDto>>> CreateAsync(DesignationDto entity)
+        {
+            var department = await _departmentService.CreateAsync(entity);
+            if (department.IsSuccess)
+                return Ok(department); // Status Code : 200
+
+            return BadRequest(department); // Status code : 400
+        }
+
+        [ClaimRequirement("Permission", new string[] { Permissions.DesignationClaims.View, Permissions.DesignationClaims.Delete, Permissions.DesignationClaims.Edit })]
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Response<DesignationDto>>> GetByIdAsync(int id)
+        {
+            var result = await _departmentService.GetByIdAsync(id);
+            if (result.IsSuccess)
+                return Ok(result); // Status Code : 200
+
+            return BadRequest(result); // Status code : 400
+        }
+
+        [ClaimRequirement("Permission", new string[] { Permissions.DesignationClaims.Edit })]
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<Response<DesignationDto>>> UpdateAsync(int id, DesignationDto entity)
+        {
+            if (id != entity.Id)
+                return BadRequest("ID mismatch");
+
+            var result = await _departmentService.UpdateAsync(entity);
+            if (result.IsSuccess)
+                return Ok(result); // Status Code : 200
+
+            return BadRequest(result); // Status code : 400
+        }
+
+        [HttpGet("Dropdown")]
+        public async Task<ActionResult<Response<List<DesignationDto>>>> GetDesignationDropDown()
+        {
+            return Ok(await _departmentService.GetDesignationDropDown()); // Status Code : 200
+        }
+    }
+}
