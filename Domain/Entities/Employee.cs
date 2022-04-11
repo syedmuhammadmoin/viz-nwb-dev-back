779bc3412c0b4a08ec4745642bd07f079aa0560f
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-04-11 13:02:37 +0500

    Added Employee model

---
+++ Domain/Entities/Employee.cs
@@ -0,0 +1,61 @@
+﻿using Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Employee : BaseEntity<int>
+    {
+        [MaxLength(100)]
+        public string Name { get; private set; }
+        [MaxLength(100)]
+        public string FatherName { get; private set; }
+        [MaxLength(20)]
+        public string CNIC { get; private set; }
+        [MaxLength(100)]
+        public string Domicile { get; private set; }
+        [MaxLength(20)]
+        public string Contact { get; private set; }
+        [MaxLength(20)]
+        public string Religion { get; private set; }
+        [MaxLength(50)]
+        public string Nationality { get; private set; }
+        [MaxLength(50)]
+        public string Maritalstatus { get; private set; }
+        [MaxLength(20)]
+        public string Gender { get; private set; }
+        [MaxLength(50)]
+        public string PlaceofBirth { get; private set; }
+        public string DesignationId { get; private set; }
+        [ForeignKey("DesignationId")] 
+        public Designation Designation { get; private set; }
+        public string DepartmentId { get; private set; }
+        [ForeignKey("DepartmentId")]
+        public Department Department { get; private set; }
+        [MaxLength(300)]
+        public string Address { get; private set; }
+        public DateTime DateofJoining { get; private set; }
+        public DateTime DateofRetirment { get; private set; }
+        public DateTime DateofBirth { get; private set; }
+        public int EarnedLeaves { get; private set; }
+        public int CasualLeaves { get; private set; }
+        [MaxLength(30)]
+        public string Status { get; private set; }
+        [MaxLength(50)]
+        public string Role { get; private set; }
+        [MaxLength(80)]
+        public string Faculty { get; private set; }
+        [MaxLength(80)]
+        public string DutyShift { get; private set; }
+
+        protected Employee()
+        {
+
+        }
+    }
+}
