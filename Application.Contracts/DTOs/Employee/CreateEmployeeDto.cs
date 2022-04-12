9caaaf57273620d3e39e75fc67ffb7f98046f22e
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-04-12 12:50:48 +0500

    Added Employee Dto

---
+++ Application.Contracts/DTOs/Employee/CreateEmployeeDto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs.Employee
+{
+    public class CreateEmployeeDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string FatherName { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string CNIC { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Domicile { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Contact { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Religion { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Nationality { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Maritalstatus { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Gender { get; set; }
+        [MaxLength(50)]
+        public string PlaceofBirth { get; set; }
+        [Required]
+        public int DesignationId { get; set; }
+        [Required]
+        public int DepartmentId { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string Address { get; set; }
+        [Required]
+        public DateTime DateofJoining { get; set; }
+        public DateTime? DateofRetirment { get; set; }
+        public DateTime? DateofBirth { get; set; }
+        public int? EarnedLeaves { get; set; }
+        public int? CasualLeaves { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Status { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Role { get; set; }
+        [Required]
+        [MaxLength(80)]
+        public string Faculty { get; set; }
+        [Required]
+        [MaxLength(80)]
+        public string DutyShift { get; set; }
+
+    }
+}
---
+++ Application.Contracts/DTOs/Employee/EmployeeDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs.Employee
+{
+    public class EmployeeDto
+    {
+        public string Name { get; private set; }
+        public string FatherName { get; private set; }
+        public string CNIC { get; private set; }
+        public string Domicile { get; private set; }
+        public string Contact { get; private set; }
+        public string Religion { get; private set; }
+        public string Nationality { get; private set; }
+        public string Maritalstatus { get; private set; }
+        public string Gender { get; private set; }
+        public string PlaceofBirth { get; private set; }
+        public int DesignationId { get; private set; }
+        public string DesignationName { get; private set; }
+        public int DepartmentId { get; private set; }
+        public string DepartmentName { get; private set; }
+        public string Address { get; private set; }
+        public DateTime DateofJoining { get; private set; }
+        public DateTime DateofRetirment { get; private set; }
+        public DateTime DateofBirth { get; private set; }
+        public int EarnedLeaves { get; private set; }
+        public int CasualLeaves { get; private set; }
+        public string Status { get; private set; }
+        public string Role { get; private set; }
+        public string Faculty { get; private set; }
+        public string DutyShift { get; private set; }
+    }
+}
