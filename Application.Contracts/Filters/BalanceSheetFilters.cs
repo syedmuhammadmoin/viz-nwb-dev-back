993908164062cf522f2db91f200c7c45d285b7ec
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-04-18 13:07:16 +0500

    Balance sheet filters

---
+++ Application.Contracts/Filters/BalanceSheetFilters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Filters
+{
+    public class BalanceSheetFilters
+    {
+        public DateTime DocDate { get; set; }
+        public string AccountName { get; set; }
+        public string CampusName { get; set; }
+    }
+}
