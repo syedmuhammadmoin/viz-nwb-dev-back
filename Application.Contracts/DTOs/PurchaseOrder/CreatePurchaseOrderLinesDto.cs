501b6f037fdbee21c9ce231144df696b6db6706a
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-04-19 14:29:13 +0500

    Added Purchase Order Dto

---
+++ Application.Contracts/DTOs/PurchaseOrder/CreatePurchaseOrderLinesDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class CreatePurchaseOrderLinesDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public Guid AccountId { get; set; }
+        [Required]
+        public int ItemId { get; set; }
+        [StringLength(200)]
+        public string Description { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than 0")]
+        public int Quantity { get; set; }
+        [Required]
+        [Range(1, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+        public decimal Cost { get; set; }
+        [Range(0.00, 100.00, ErrorMessage = "Please enter a positive value")]
+        public decimal Tax { get; set; }
+        [Required]
+        public int WarehouseId { get; set; }
+    }
+}
---
+++ Application.Contracts/DTOs/PurchaseOrder/PurchaseOrderLinesDto.cs
@@ -0,0 +1,24 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class PurchaseOrderLinesDto
+    {
+        public int Id { get; private set; }
+        public int ItemId { get; private set; }
+        public string Item { get; private set; }
+        public string Description { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal Cost { get; private set; }
+        public decimal Tax { get; private set; }
+        public Guid AccountId { get; private set; }
+        public string AccountName { get; private set; }
+        public int? WarehouseId { get; private set; }
+        public string Warehouse { get; private set; }
+        public int MasterId { get; private set; }
+    }
+}
