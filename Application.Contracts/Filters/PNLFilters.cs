a8ee427efd13e6e5e64906f01b622cc973ab8c93
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-04-20 14:17:58 +0500

    Added PNL Filter

---
+++ Application.Contracts/Filters/PNLFilters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Filters
+{
+    public class PNLFilters
+    {
+        public string AccountName { get; set; }
+        [Required]
+        public DateTime DocDate { get; set; }
+        [Required]
+        public DateTime DocDate2 { get; set; }
+        public string BusinessPartner { get; set; }
+        public string Warehouse { get; set; }
+        public string Location { get; set; }
+    }
+}
