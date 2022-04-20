ae738abada4fa886f9c8ef4f003ef6f99fef6b24
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-04-20 14:59:10 +0500

    Added Permission for PNL Report

--- Domain/Constants/Permissions.cs
+++ Domain/Constants/Permissions.cs
@@ -230,5 +230,9 @@ namespace Domain.Constants
         {
             public const string View = "Permissions.GeneralLedgerClaims.View";
         }
+        public static class ProfitLossClaims
+        {
+            public const string View = "Permissions.ProfitLossClaims.View";
+        }
     }
 }
