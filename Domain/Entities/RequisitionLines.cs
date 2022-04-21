44c2018379ff08957c98f9371c789cf3d1c6630e
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-04-21 15:51:40 +0500

    fixed

--- Domain/Entities/RequisitionLines.cs
+++ Domain/Entities/RequisitionLines.cs
@@ -16,12 +16,10 @@ namespace Domain.Entities
         public Product Item { get; private set; }
         [MaxLength(100)]
         public string Description { get; private set; }
-        [MaxLength(20)]
         public int Quantity { get; private set; }
         public int? WarehouseId { get; private set; }
         [ForeignKey("WarehouseId")]
         public Warehouse Warehouse { get; private set; }
-        [Column(TypeName = "decimal(18,2)")]
         public int MasterId { get; private set; }
         [ForeignKey("MasterId")]
         public RequisitionMaster RequisitionMaster { get; private set; }
