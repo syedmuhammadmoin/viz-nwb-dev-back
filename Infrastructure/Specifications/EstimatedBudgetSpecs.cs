31b1b2434f0d971e05e5f22d33af39e6bb7b560f
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-04-25 16:18:33 +0500

    Added Estimated Budget Specs

---
+++ Infrastructure/Specifications/EstimatedBudgetSpecs.cs
@@ -0,0 +1,34 @@
+﻿using Application.Contracts.Filters;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Specifications
+{
+    public class EstimatedBudgetSpecs : BaseSpecification<EstimatedBudgetMaster>
+    {
+        public EstimatedBudgetSpecs(PaginationFilter filter)
+        {
+            var validFilter = new PaginationFilter(filter.PageStart, filter.PageEnd);
+            ApplyPaging(validFilter.PageStart, validFilter.PageEnd - validFilter.PageStart);
+            AddInclude(i=> i.PreviousBudget);
+            AddInclude("EstimatedBudgetLines.Account");
+        }
+        public EstimatedBudgetSpecs(bool forEdit)
+        {
+            if (forEdit)
+            {
+                AddInclude(i => i.PreviousBudget);
+                AddInclude(i => i.EstimatedBudgetLines);
+            }
+            else
+            {
+                AddInclude(i => i.PreviousBudget);
+                AddInclude("EstimatedBudgetLines.Account");
+            }
+        }
+    }
+}
