4d1efa8efdaef87a197c0f9efbbcb3fb9df1a21d
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-05-13 19:05:56 +0500

    Updated BankStmtDto and added UnReconDto

---
+++ Application.Contracts/DTOs/BankStmt/UnReconStmtDto.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class UnReconStmtDto
+    {
+        public int Id { get; set; }
+        public string DocNo { get; set; }
+        public DateTime DocDate { get; set; }
+        public decimal Amount { get; set; }
+        public decimal ReconciledAmount { get; set; }
+        public decimal UnreconciledAmount { get; set; }
+        public string Label { get; set; }
+        public string Description { get; set; }
+        public DocumentStatus BankReconStatus { get; set; }
+    }
+}
