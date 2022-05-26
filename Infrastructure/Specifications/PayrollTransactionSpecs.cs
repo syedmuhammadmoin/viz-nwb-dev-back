0986128b845e64945422c5744f368cec24fc5d3a
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-05-26 15:03:31 +0500

    updated paginationResponse

--- Infrastructure/Specifications/PayrollTransactionSpecs.cs
+++ Infrastructure/Specifications/PayrollTransactionSpecs.cs
@@ -15,6 +15,7 @@ namespace Infrastructure.Specifications
         {
             var validFilter = new PaginationFilter(filter.PageStart, filter.PageEnd);
             ApplyPaging(validFilter.PageStart, validFilter.PageEnd - validFilter.PageStart);
+            ApplyOrderByDescending(i => i.Id);
             AddInclude(a => a.AccountPayable);
             AddInclude(a => a.Department);
             AddInclude(a => a.Designation);
