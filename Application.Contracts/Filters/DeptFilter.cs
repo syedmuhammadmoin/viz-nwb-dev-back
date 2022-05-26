9b94ab03dde455768593e11bcbec19cdf4707836
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-05-26 15:52:30 +0500

    added DeptFilter for payrollProcess

---
+++ Application.Contracts/Filters/DeptFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Filters
+{
+    public class DeptFilter
+    {
+        public int?[] DepartmentId { get; set; }
+        [Required]
+        public int Month { get; set; }
+        [Required]
+        public int Year { get; set; }
+        public int? BankId { get; set; }
+        public Guid AccountPayableId { get; set; }
+    }
+}
