e2381e2559afaab23a518610831293eb8f497311
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-05-27 18:35:12 +0500

    Added specs for payrollReport

--- Infrastructure/Specifications/PayrollTransactionSpecs.cs
+++ Infrastructure/Specifications/PayrollTransactionSpecs.cs
@@ -64,22 +64,6 @@ namespace Infrastructure.Specifications
 
         }
 
-        //public PayrollTransactionSpecs(int month, int year, int?[] departmentIds, bool isPayrollTransactoin)
-        //    : base(x => x.Month == month && x.Year == year
-        //        && (departmentIds.Count() > 0 ? departmentIds.Contains(x.DepartmentId) : true)
-        //        && (x.Status.State == DocumentStatus.Draft || x.Status.State == DocumentStatus.Rejected)
-        //        && isPayrollTransactoin == true)
-        //{
-        //    AddInclude(a => a.AccountPayable);
-        //    AddInclude(a => a.Department);
-        //    AddInclude(a => a.Designation);
-        //    AddInclude(a => a.Employee);
-        //    AddInclude(i => i.PayrollTransactionLines);
-        //    AddInclude(i => i.Status);
-        //    AddInclude("PayrollTransactionLines.PayrollItem");
-        //    AddInclude("PayrollTransactionLines.Account");
-        //}
-
         public PayrollTransactionSpecs(int month, int year, int?[] departmentIds, bool isPayrollTransactoin)
             : base(x => x.Month == month && x.Year == year
                 && (departmentIds.Count() > 0 ? departmentIds.Contains(x.DepartmentId) : true)
@@ -94,6 +78,29 @@ namespace Infrastructure.Specifications
             AddInclude(i => i.Status);
             AddInclude("PayrollTransactionLines.PayrollItem");
             AddInclude("PayrollTransactionLines.Account");
+        }
+
+        public PayrollTransactionSpecs(List<int?> months, List<int?> years, List<int?> employees,
+            DateTime fromDate, DateTime toDate, string designation, string department, string bps)
+            : base(x =>
+                ((((DateTime)x.ModifiedDate).Date >= fromDate && ((DateTime)x.ModifiedDate).Date <= toDate))
+                && (months.Count() > 0 ? months.Contains(x.Month) : true)
+                && (years.Count() > 0 ? years.Contains(x.Year) : true)
+                && (employees.Count() > 0 ? employees.Contains(x.EmployeeId) : true)
+                && x.Designation.Name.Contains(designation != null ? designation : "")
+                && x.Department.Name.Contains(department != null ? department : "")
+                && x.BPSName.Contains(bps != null ? bps : "")
+                && (x.Status.State != DocumentStatus.Draft && x.Status.State != DocumentStatus.Cancelled && x.Status.State != DocumentStatus.Rejected)
+                )
+        {
+            AddInclude(a => a.AccountPayable);
+            AddInclude(a => a.Department);
+            AddInclude(a => a.Designation);
+            AddInclude(a => a.Employee);
+            AddInclude(i => i.Status);
+            AddInclude("PayrollTransactionLines.PayrollItem");
+            AddInclude("PayrollTransactionLines.Account");
+            ApplyOrderByDescending(i => i.Id);
         }
     }
 }
