11ff6a8f74c6c5695e00f6979ad0f1f4d817ad09
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-05-27 18:34:56 +0500

    Added PayrollFilter for payrollReport

---
+++ Application.Contracts/Filters/PayrollFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Filters
+{
+    public class PayrollFilter
+    {
+        public int? EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Designation { get; set; }
+        public string Department { get; set; }
+        public string BPS { get; set; }
+        public int? Month { get; set; }
+        public int? Year { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+}
