5a05bc76599c9f5676a24fb9288808d17204cef5
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-06-01 19:50:05 +0500

    updated specs

---
+++ Infrastructure/Specifications/PayrollPaymentSpecs.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Constants;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Specifications
+{
+    public class PayrollPaymentSpecs : BaseSpecification<Payment>
+    {
+        public PayrollPaymentSpecs() : base(x => x.Status.State == DocumentStatus.Draft || x.Status.State == DocumentStatus.Rejected)
+        {
+            AddInclude(i => i.Status);
+            AddInclude(i => i.BusinessPartner);
+        }
+    }
+}
