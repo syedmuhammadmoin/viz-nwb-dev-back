0da1e95d7505ba23695bcdc5eb31b3797f8565b4
Waleed Tariq <dev.ratech@outlook.com>
2022-06-04 00:16:18 +0500

    add payment amount dto

---
+++ Application.Contracts/DTOs/TransactionRecon/PaymentAmountListDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class PaymentAmountListDto
+    {
+        public int LedgerId { get; set; }
+        public decimal UnreconciledAmount { get; set; }
+    }
+}
