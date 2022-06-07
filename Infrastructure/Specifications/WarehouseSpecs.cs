307ae97aa9b0ea8d3ad18b4eab5b2bb33eadd55a
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-06-07 15:50:19 +0500

    updated service

--- Infrastructure/Specifications/WarehouseSpecs.cs
+++ Infrastructure/Specifications/WarehouseSpecs.cs
@@ -11,7 +11,7 @@ namespace Infrastructure.Specifications
     public class WarehouseSpecs : BaseSpecification<Warehouse>
     {
         public WarehouseSpecs(TransactionFormFilter filter) 
-            : base(c=> c.Name.Contains(filter.DocNo != null ? filter.DocNo : ""))
+            : base(c=> c.Name.Contains(filter.Name != null ? filter.Name : ""))
         {
             var validFilter = new PaginationFilter(filter.PageStart, filter.PageEnd);
             ApplyPaging(validFilter.PageStart, validFilter.PageEnd - validFilter.PageStart);
