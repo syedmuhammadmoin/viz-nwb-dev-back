6790586023d376dc467d7a300eba36fd2e9fb5d7
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-06-07 15:38:14 +0500

    updated

--- Application.Contracts/Filters/TransactionFormFilter.cs
+++ Application.Contracts/Filters/TransactionFormFilter.cs
@@ -13,12 +13,13 @@ namespace Application.Contracts.Filters
         public string BusinessPartner { get; set; }
         public string Department { get; set; }
         public string Designation { get; set; }
+        public string Warehouse { get; set; }
+        public string Name { get; set; }
+        public string Account { get; set; }
+        public string Category { get; set; }
         public DateTime? DocDate { get; set; }
         public DateTime? DueDate { get; set; }
         public DocumentStatus? State { get; set; }
-        public decimal? Amount { get; set; }
         public bool? isActive { get; set; }
-
-
     }
 }
