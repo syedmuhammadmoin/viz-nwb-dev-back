9d2a348f25f91d760e3689f43b4c9265d694ccea
Waleed Tariq <dev.ratech@outlook.com>
2022-06-13 19:09:23 +0500

    Segerigrate methods of transaction recon

--- Application/Services/TransactionReconcileService.cs
+++ Application/Services/TransactionReconcileService.cs
@@ -25,60 +25,22 @@ namespace Application.Services
 
         public async Task<Response<bool>> Reconcile(CreateTransactionReconcileDto entity)
         {
-            //Checking if both id are same
-            if (entity.DocumentLedgerId == entity.PaymentLedgerId)
-                return new Response<bool>("Both Ledger id cannot be same");
+            //Checking Reconciliation Validation
+            var checkValidation = CheckReconValidation(entity);
+            if (!checkValidation.IsSuccess)
+                return new Response<bool>(checkValidation.Message);
 
-            //Getting transaction with Payment Transaction Id
-            var getUnreconciledDocumentAmount = _unitOfWork.Ledger.Find(new LedgerSpecs(entity.DocumentLedgerId)).FirstOrDefault();
-            if (getUnreconciledDocumentAmount == null)
-                return new Response<bool>("No Transaction found for the given document transaction id");
-            
-            // Checking if given amount is greater than unreconciled document amount
-            var reconciledDocumentAmount = _unitOfWork.TransactionReconcile.Find(new TransactionReconSpecs(entity.DocumentLedgerId, false)).Sum(p => p.Amount);
-            var unreconciledDocumentAmount = getUnreconciledDocumentAmount.Amount - reconciledDocumentAmount;
-            if (entity.Amount > unreconciledDocumentAmount)
-                return new Response<bool>("Enter amount is greater than pending document amount");
-
-            //Getting transaction with Document Transaction Id
-            var getUnreconciledPaymentAmount = _unitOfWork.Ledger.Find(new LedgerSpecs(entity.PaymentLedgerId,
-                getUnreconciledDocumentAmount.Level4_id, getUnreconciledDocumentAmount.BusinessPartnerId, getUnreconciledDocumentAmount.Sign)).FirstOrDefault();
-            if (getUnreconciledDocumentAmount == null)
-                return new Response<bool>("No Transaction found for the given payment transaction id");
-            
-            // Checking if given amount is greater than unreconciled payment amount
-            var reconciledPaymentAmount = _unitOfWork.TransactionReconcile.Find(new TransactionReconSpecs(entity.PaymentLedgerId, true)).Sum(p => p.Amount);
-            var unreconciledPaymentAmount = getUnreconciledPaymentAmount.Amount - reconciledPaymentAmount;
-            if (entity.Amount > unreconciledPaymentAmount)
-                return new Response<bool>("Enter amount is greater than pending payment amount");
 
             //Begin Transaction
             _unitOfWork.CreateTransaction();
             try
             {
-                //Adding in Reconcilation table
-                var recons = new TransactionReconcile(entity.PaymentLedgerId, entity.DocumentLedgerId, entity.Amount);
-                await _unitOfWork.TransactionReconcile.Reconcile(recons);
-                await _unitOfWork.SaveAsync();
-
-                //Get Paymet Total Reconciled Amount
-                var reconciledTotalPayment = _unitOfWork.TransactionReconcile
-                    .Find(new TransactionReconSpecs(entity.PaymentLedgerId, true))
-                    .Sum(i => i.Amount);
-
-                //FOR UPDATE STATUS
-                await UpdateStatus(getUnreconciledPaymentAmount, reconciledTotalPayment);
-                await _unitOfWork.SaveAsync();
-
-                //Get Document Total Reconciled Amount
-                var reconciledTotalDocAmount = _unitOfWork.TransactionReconcile
-                    .Find(new TransactionReconSpecs(getUnreconciledDocumentAmount.Id, false))
-                    .Sum(i => i.Amount);
-
-                //FOR UPDATE STATUS
-                await UpdateStatus(getUnreconciledDocumentAmount, reconciledTotalDocAmount);
-
-                await _unitOfWork.SaveAsync();
+                var reconcile = await ReconciliationProcess(entity);
+                if (!reconcile.IsSuccess)
+                {
+                    _unitOfWork.Rollback();
+                    return new Response<bool>(reconcile.Message);
+                }
                 _unitOfWork.Commit();
 
                 return new Response<bool>(true, "Reconciled Successfully");
@@ -90,134 +52,154 @@ namespace Application.Services
             }
         }
 
-        private async Task UpdateStatus(RecordLedger ledger, decimal reconciledTotalAmount)
+
+        public Response<bool> CheckReconValidation(CreateTransactionReconcileDto entity)
         {
+            //Checking if both id are same
+            if (entity.DocumentLedgerId == entity.PaymentLedgerId)
+                return new Response<bool>("Both Ledger id cannot be same");
+
+            //Getting transaction with Payment Transaction Id
+            var getUnreconciledDocumentAmount = _unitOfWork.Ledger.Find(new LedgerSpecs(entity.DocumentLedgerId)).FirstOrDefault();
+            if (getUnreconciledDocumentAmount == null)
+                return new Response<bool>("No Transaction found for the given document transaction id");
+
+            // Checking if given amount is greater than unreconciled document amount
+            var reconciledDocumentAmount = _unitOfWork.TransactionReconcile.Find(new TransactionReconSpecs(entity.DocumentLedgerId, false)).Sum(p => p.Amount);
+            var unreconciledDocumentAmount = getUnreconciledDocumentAmount.Amount - reconciledDocumentAmount;
+            if (entity.Amount > unreconciledDocumentAmount)
+                return new Response<bool>("Enter amount is greater than pending document amount");
+
+            //Getting transaction with Document Transaction Id
+            var getUnreconciledPaymentAmount = _unitOfWork.Ledger.Find(new LedgerSpecs(entity.PaymentLedgerId,
+                getUnreconciledDocumentAmount.Level4_id, getUnreconciledDocumentAmount.BusinessPartnerId, getUnreconciledDocumentAmount.Sign)).FirstOrDefault();
+            if (getUnreconciledDocumentAmount == null)
+                return new Response<bool>("No Transaction found for the given payment transaction id");
+
+            // Checking if given amount is greater than unreconciled payment amount
+            var reconciledPaymentAmount = _unitOfWork.TransactionReconcile.Find(new TransactionReconSpecs(entity.PaymentLedgerId, true)).Sum(p => p.Amount);
+            var unreconciledPaymentAmount = getUnreconciledPaymentAmount.Amount - reconciledPaymentAmount;
+            if (entity.Amount > unreconciledPaymentAmount)
+                return new Response<bool>("Enter amount is greater than pending payment amount");
+
+
+            return new Response<bool>(true, "No validation error found");
+        }
+
+        public async Task<Response<bool>> ReconciliationProcess(CreateTransactionReconcileDto entity)
+        {
+            //Adding in Reconcilation table
+            var recons = new TransactionReconcile(entity.PaymentLedgerId, entity.DocumentLedgerId, entity.Amount);
+            await _unitOfWork.TransactionReconcile.Reconcile(recons);
+            await _unitOfWork.SaveAsync();
+
+            //Get Paymet Total Reconciled Amount
+            var reconciledTotalPayment = _unitOfWork.TransactionReconcile
+                .Find(new TransactionReconSpecs(entity.PaymentLedgerId, true))
+                .Sum(i => i.Amount);
+
             //FOR UPDATE STATUS
-            if (ledger.Amount == reconciledTotalAmount)
+            await UpdateStatus(entity.PaymentLedgerId, reconciledTotalPayment);
+            await _unitOfWork.SaveAsync();
+
+            //Get Document Total Reconciled Amount
+            var reconciledTotalDocAmount = _unitOfWork.TransactionReconcile
+                .Find(new TransactionReconSpecs(entity.DocumentLedgerId, false))
+                .Sum(i => i.Amount);
+
+            //FOR UPDATE STATUS
+            await UpdateStatus(entity.DocumentLedgerId, reconciledTotalDocAmount);
+
+            await _unitOfWork.SaveAsync();
+
+            return new Response<bool>(true, "Reconciled Successfully");
+        }
+
+        private async Task UpdateStatus(int ledgerId, decimal reconciledTotalDocument)
+        {
+            var updateLedger = await _unitOfWork.Ledger.GetById(ledgerId, new GetLedgerByIdSpecs());
+          
+            //FOR UPDATE STATUS
+            if (updateLedger.Amount == reconciledTotalDocument)
             {
-                var updateLedger = await _unitOfWork.Ledger.GetById(ledger.Id);
                 updateLedger.setStatus(DocumentStatus.Reconciled);
-                switch (ledger.Transactions.DocType)
+                switch (updateLedger.Transactions.DocType)
                 {
                     case DocType.Payment:
-                        var payment = _unitOfWork.Payment.Find(new PaymentSpecs(ledger.TransactionId)).FirstOrDefault();
+                        var payment = _unitOfWork.Payment.Find(new PaymentSpecs(updateLedger.TransactionId)).FirstOrDefault();
                         payment.setStatus(5); // Paid
                         break;
                     case DocType.Receipt:
-                        var receipt = _unitOfWork.Payment.Find(new PaymentSpecs(ledger.TransactionId)).FirstOrDefault();
+                        var receipt = _unitOfWork.Payment.Find(new PaymentSpecs(updateLedger.TransactionId)).FirstOrDefault();
                         receipt.setStatus(5); // Paid
                         break;
                     case DocType.PayrollPayment:
-                        var payrollPayment = _unitOfWork.Payment.Find(new PaymentSpecs(ledger.TransactionId)).FirstOrDefault();
+                        var payrollPayment = _unitOfWork.Payment.Find(new PaymentSpecs(updateLedger.TransactionId)).FirstOrDefault();
                         payrollPayment.setStatus(5); // Paid
                         break;
                     case DocType.CreditNote:
-                        var creaditNote = _unitOfWork.CreditNote.Find(new CreditNoteSpecs(ledger.TransactionId)).FirstOrDefault();
+                        var creaditNote = _unitOfWork.CreditNote.Find(new CreditNoteSpecs(updateLedger.TransactionId)).FirstOrDefault();
                         creaditNote.setStatus(5); // Paid
                         break;
                     case DocType.DebitNote:
-                        var debitNote = _unitOfWork.DebitNote.Find(new DebitNoteSpecs(ledger.TransactionId)).FirstOrDefault();
+                        var debitNote = _unitOfWork.DebitNote.Find(new DebitNoteSpecs(updateLedger.TransactionId)).FirstOrDefault();
                         debitNote.setStatus(5); // Paid
                         break;
                     case DocType.Invoice:
-                        var invoice = _unitOfWork.Invoice.Find(new InvoiceSpecs(ledger.TransactionId)).FirstOrDefault();
+                        var invoice = _unitOfWork.Invoice.Find(new InvoiceSpecs(updateLedger.TransactionId)).FirstOrDefault();
                         invoice.setStatus(5); // Paid
                         break;
                     case DocType.Bill:
-                        var bill = _unitOfWork.Bill.Find(new BillSpecs(ledger.TransactionId)).FirstOrDefault();
+                        var bill = _unitOfWork.Bill.Find(new BillSpecs(updateLedger.TransactionId)).FirstOrDefault();
                         bill.setStatus(5); // Paid
                         break;
                     case DocType.PayrollTransaction:
-                        var payrollTransaction = _unitOfWork.PayrollTransaction.Find(new PayrollTransactionSpecs(ledger.TransactionId)).FirstOrDefault();
+                        var payrollTransaction = _unitOfWork.PayrollTransaction.Find(new PayrollTransactionSpecs(updateLedger.TransactionId)).FirstOrDefault();
                         payrollTransaction.setStatus(5); // Paid
                         break;
                 }
             }
             else
             {
-                var updateLedger = await _unitOfWork.Ledger.GetById(ledger.Id);
                 updateLedger.setStatus(DocumentStatus.Partial);
-                switch (ledger.Transactions.DocType)
+                switch (updateLedger.Transactions.DocType)
                 {
                     case DocType.Payment:
-                        var payment = _unitOfWork.Payment.Find(new PaymentSpecs(ledger.TransactionId)).FirstOrDefault();
+                        var payment = _unitOfWork.Payment.Find(new PaymentSpecs(updateLedger.TransactionId)).FirstOrDefault();
                         payment.setStatus(4); // Partial
                         break;
                     case DocType.Receipt:
-                        var receipt = _unitOfWork.Payment.Find(new PaymentSpecs(ledger.TransactionId)).FirstOrDefault();
+                        var receipt = _unitOfWork.Payment.Find(new PaymentSpecs(updateLedger.TransactionId)).FirstOrDefault();
                         receipt.setStatus(4); // Partial
                         break;
                     case DocType.PayrollPayment:
-                        var payrollPayment = _unitOfWork.Payment.Find(new PaymentSpecs(ledger.TransactionId)).FirstOrDefault();
+                        var payrollPayment = _unitOfWork.Payment.Find(new PaymentSpecs(updateLedger.TransactionId)).FirstOrDefault();
                         payrollPayment.setStatus(4); // Partial
                         break;
                     case DocType.CreditNote:
-                        var creaditNote = _unitOfWork.CreditNote.Find(new CreditNoteSpecs(ledger.TransactionId)).FirstOrDefault();
+                        var creaditNote = _unitOfWork.CreditNote.Find(new CreditNoteSpecs(updateLedger.TransactionId)).FirstOrDefault();
                         creaditNote.setStatus(4); // Partial
                         break;
                     case DocType.DebitNote:
-                        var debitNote = _unitOfWork.DebitNote.Find(new DebitNoteSpecs(ledger.TransactionId)).FirstOrDefault();
+                        var debitNote = _unitOfWork.DebitNote.Find(new DebitNoteSpecs(updateLedger.TransactionId)).FirstOrDefault();
                         debitNote.setStatus(4); // Partial
                         break;
                     case DocType.Invoice:
-                        var invoice = _unitOfWork.Invoice.Find(new InvoiceSpecs(ledger.TransactionId)).FirstOrDefault();
+                        var invoice = _unitOfWork.Invoice.Find(new InvoiceSpecs(updateLedger.TransactionId)).FirstOrDefault();
                         invoice.setStatus(4); // Partial
                         break;
                     case DocType.Bill:
-                        var bill = _unitOfWork.Bill.Find(new BillSpecs(ledger.TransactionId)).FirstOrDefault();
+                        var bill = _unitOfWork.Bill.Find(new BillSpecs(updateLedger.TransactionId)).FirstOrDefault();
                         bill.setStatus(4); // Partial
                         break;
                     case DocType.PayrollTransaction:
-                        var payrollTransaction = _unitOfWork.PayrollTransaction.Find(new PayrollTransactionSpecs(ledger.TransactionId)).FirstOrDefault();
+                        var payrollTransaction = _unitOfWork.PayrollTransaction.Find(new PayrollTransactionSpecs(updateLedger.TransactionId)).FirstOrDefault();
                         payrollTransaction.setStatus(4); // Partial
                         break;
                 }
             }
         }
 
-        //Will do it later
-        private Response<List<PaymentAmountListDto>> GetPaymentAmountListDto(int paymentTransactionId, int documentTransactionId, decimal amount)
-        {
-            //Checking if both id are same
-            if (documentTransactionId == paymentTransactionId)
-                return new Response<List<PaymentAmountListDto>>("Both transaction id cannot be same");
-
-            //Getting transaction with Payment Transaction Id
-            var getUnreconciledDocumentAmount = _unitOfWork.Ledger.Find(new LedgerSpecs(documentTransactionId)).FirstOrDefault();
-            if (getUnreconciledDocumentAmount == null)
-                return new Response<List<PaymentAmountListDto>>("No Transaction found for the given document transaction id");
-
-            // Checking if given amount is greater than unreconciled document amount
-            var reconciledDocumentAmount = _unitOfWork.TransactionReconcile.Find(new TransactionReconSpecs(getUnreconciledDocumentAmount.Id, false)).Sum(p => p.Amount);
-            var unreconciledDocumentAmount = getUnreconciledDocumentAmount.Amount - reconciledDocumentAmount;
-            if (amount > unreconciledDocumentAmount)
-                return new Response<List<PaymentAmountListDto>>("Enter amount is greater than pending document amount");
-
-
-            //Getting transaction with Document Transaction Id
-            var getUnreconciledPaymentAmount = _unitOfWork.Ledger.Find(new LedgerSpecs(paymentTransactionId,
-                getUnreconciledDocumentAmount.Level4_id, getUnreconciledDocumentAmount.BusinessPartnerId, getUnreconciledDocumentAmount.Sign)).ToList();
-            if (!getUnreconciledPaymentAmount.Any())
-                return new Response<List<PaymentAmountListDto>>("No Transaction found for the given payment transaction id");
-
-            // declaring list for unconciledPaymentAmounts
-            var unreconciledPaymentAmountList = new List<PaymentAmountListDto>();
-            decimal unreconciledPaymentAmountTotal = 0;
-
-            // looping thourgh unreconciled amount for adding them to list
-            foreach (var item in getUnreconciledPaymentAmount)
-            {
-                var reconciledPaymentAmount = _unitOfWork.TransactionReconcile.Find(new TransactionReconSpecs(item.Id, true)).Sum(p => p.Amount);
-                unreconciledPaymentAmountTotal += item.Amount - reconciledPaymentAmount;
-
-                unreconciledPaymentAmountList.Add(new PaymentAmountListDto()
-                {
-                    LedgerId = item.Id,
-                    UnreconciledAmount = item.Amount - reconciledPaymentAmount,
-                });
-            }
-            return new Response<List<PaymentAmountListDto>>(unreconciledPaymentAmountList, "Returning List");
-        }
 
         public Response<List<AmountsForReconciliationDto>> GetPaymentReconAmounts(Guid accountId, int businessPartnerId, char sign)
         {
