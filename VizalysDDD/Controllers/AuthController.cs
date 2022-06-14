ce253947c8efa42ef1721068cf1cd03f23138865
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-06-14 17:35:42 +0500

    Added Workflow permissions in GetRolesAsync so that user can see workflow without access management permission

--- VizalysDDD/Controllers/AuthController.cs
+++ VizalysDDD/Controllers/AuthController.cs
@@ -127,7 +127,7 @@ namespace Vizalys.Api.Controllers
             return BadRequest(result);// Status code : 400
         }
 
-        [ClaimRequirement("Permission", new string[] { Permissions.AuthClaims.Create, Permissions.AuthClaims.View, Permissions.AuthClaims.Delete, Permissions.AuthClaims.Edit })]
+        [ClaimRequirement("Permission", new string[] { Permissions.WorkflowClaims.Create, Permissions.WorkflowClaims.View, Permissions.WorkflowClaims.Delete, Permissions.WorkflowClaims.Edit, Permissions.AuthClaims.Create, Permissions.AuthClaims.View, Permissions.AuthClaims.Delete, Permissions.AuthClaims.Edit })]
         [HttpGet("Roles")]
         public async Task<ActionResult<Response<IEnumerable<IdentityRole>>>> GetRolesAsync()
         {
