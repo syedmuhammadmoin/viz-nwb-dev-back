63a384e83e3027783275d5a04c3516e1eff52f43
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-06-20 17:13:19 +0500

    Added TaxDtos

---
+++ Application.Contracts/DTOs/Tax/TaxDto.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class TaxDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public TaxType TaxType { get; set; }
+        public Guid AccountId { get; set; }
+        public string AccountName { get; set; }
+    }
+}
