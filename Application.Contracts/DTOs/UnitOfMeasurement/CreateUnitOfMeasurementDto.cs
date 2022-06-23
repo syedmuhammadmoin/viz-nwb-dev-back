c20b474054d99dbc8bf5065626203c3113ab663d
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-06-23 12:25:36 +0500

    Added UnitOfMeasurement Dto

---
+++ Application.Contracts/DTOs/UnitOfMeasurement/CreateUnitOfMeasurementDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class CreateUnitOfMeasurementDto
+    {
+        public int? Id { get; set; }
+        [MaxLength(50)]
+        [Required] 
+        public string Name { get; set; }
+    }
+}
