13ba8eceb42b632d515c1efa26100e2e5d707d1d
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-06-23 18:28:01 +0500

    Added migration for Issuance form

---
+++ Infrastructure/Migrations/20220623123855_AddIssuanceTable.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddIssuanceTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "IssuanceMaster",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    IssuanceDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DocNo = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    CampusId = table.Column<int>(type: "int", nullable: false),
+                    StatusId = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IssuanceMaster", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IssuanceMaster_Campuses_CampusId",
+                        column: x => x.CampusId,
+                        principalTable: "Campuses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_IssuanceMaster_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_IssuanceMaster_WorkFlowStatus_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "WorkFlowStatus",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IssuanceLines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ItemId = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    WarehouseId = table.Column<int>(type: "int", nullable: true),
+                    MasterId = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IssuanceLines", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IssuanceLines_IssuanceMaster_MasterId",
+                        column: x => x.MasterId,
+                        principalTable: "IssuanceMaster",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IssuanceLines_Products_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_IssuanceLines_Warehouses_WarehouseId",
+                        column: x => x.WarehouseId,
+                        principalTable: "Warehouses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IssuanceLines_ItemId",
+                table: "IssuanceLines",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IssuanceLines_MasterId",
+                table: "IssuanceLines",
+                column: "MasterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IssuanceLines_WarehouseId",
+                table: "IssuanceLines",
+                column: "WarehouseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IssuanceMaster_CampusId",
+                table: "IssuanceMaster",
+                column: "CampusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IssuanceMaster_EmployeeId",
+                table: "IssuanceMaster",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IssuanceMaster_StatusId",
+                table: "IssuanceMaster",
+                column: "StatusId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IssuanceLines");
+
+            migrationBuilder.DropTable(
+                name: "IssuanceMaster");
+        }
+    }
+}
