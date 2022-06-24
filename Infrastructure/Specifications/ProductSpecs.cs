56b1bfe31342781b2749c6f4eabf040b52c9bf9e
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-06-24 11:38:05 +0500

    Including UnitOfMeasurement in prodcutSpecs

--- Infrastructure/Specifications/ProductSpecs.cs
+++ Infrastructure/Specifications/ProductSpecs.cs
@@ -21,10 +21,12 @@ namespace Infrastructure.Specifications
             ApplyPaging(validFilter.PageStart, validFilter.PageEnd - validFilter.PageStart);
             ApplyOrderByDescending(i => i.Id);
             AddInclude(i => i.Category);
+            AddInclude(i => i.UnitOfMeasurement);
         }
         public ProductSpecs()
         {
             AddInclude(i => i.Category);
+            AddInclude(i => i.UnitOfMeasurement);
         }
     }
 }
