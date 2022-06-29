2fc7b705ca93e30a23257ba8a1d5fc8693e04d98
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-06-29 18:31:31 +0500

    added GRNandPOReferenceDto

---
+++ Application.Contracts/DTOs/POToGRNLineReconcile/GRNAndPOReferenceDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class GRNAndPOReferenceDto
+    {
+        public int DocId { get; set; }
+    }
+}
