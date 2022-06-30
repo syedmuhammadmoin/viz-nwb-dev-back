ddadc4ecdfba9c3a721ea8442a10226ec9852536
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-06-30 19:49:02 +0500

    added stock specs

---
+++ Infrastructure/Specifications/StockSpecs.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Specifications
+{
+    public class StockSpecs : BaseSpecification<Stock>
+    {
+        public StockSpecs(int itemId, int warehouseId) : base( x => x.ItemId == itemId && x.WarehouseId == warehouseId)
+        {
+        }
+    }
+}
