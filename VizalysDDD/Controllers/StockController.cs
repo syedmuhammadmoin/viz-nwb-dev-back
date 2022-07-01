78c4a6fd473ff6682c78e5793eaba1a9de98115c
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-07-01 10:32:41 +0500

    Added Stock Controller and added services in program.cs

---
+++ VizalysDDD/Controllers/StockController.cs
@@ -0,0 +1,37 @@
+﻿using Application.Contracts.DTOs;
+using Application.Contracts.Filters;
+using Application.Contracts.Helper;
+using Application.Contracts.Interfaces;
+using Application.Contracts.Response;
+using Domain.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vizalys.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+
+    public class StockController : ControllerBase
+    {
+        private readonly IStockService _stockService;
+
+        public StockController(IStockService stockService)
+        {
+            _stockService = stockService;
+        }
+        [ClaimRequirement("Permission", new string[] { Permissions.StockClaims.View})]
+        [HttpGet]
+        public async Task<ActionResult<PaginationResponse<List<StockDto>>>> GetAllAsync([FromQuery] PaginationFilter filter)
+        {
+            var results = await _stockService.GetAllAsync(filter);
+            if (results.IsSuccess)
+                return Ok(results); // Status Code : 200
+
+            return BadRequest(results); // Status code : 400
+        }
+
+    }
+}
