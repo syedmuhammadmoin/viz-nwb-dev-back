868a0b7cff67b385f5487270f035365621204433
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-07-01 10:31:56 +0500

    added stock specs

--- Infrastructure/Specifications/StockSpecs.cs
+++ Infrastructure/Specifications/StockSpecs.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entities;
+﻿using Application.Contracts.Filters;
+using Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,15 @@ namespace Infrastructure.Specifications
 {
     public class StockSpecs : BaseSpecification<Stock>
     {
+        public StockSpecs(PaginationFilter filter)
+        {
+            var validFilter = new PaginationFilter(filter.PageStart, filter.PageEnd);
+            ApplyPaging(validFilter.PageStart, validFilter.PageEnd - validFilter.PageStart);
+            ApplyOrderByDescending(i => i.Id);
+            AddInclude(i => i.Warehouse);
+            AddInclude("Item.Category");
+            AddInclude("Item.UnitOfMeasurement");
+        }
         public StockSpecs(int itemId, int warehouseId) : base( x => x.ItemId == itemId && x.WarehouseId == warehouseId)
         {
         }
