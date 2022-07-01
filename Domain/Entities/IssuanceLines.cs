1ff4fb18f226d23615f44ddeed98c0eebe6f1c9e
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-07-01 19:00:27 +0500

    updated warehouse id to not nullable

--- Domain/Entities/IssuanceLines.cs
+++ Domain/Entities/IssuanceLines.cs
@@ -17,7 +17,7 @@ namespace Domain.Entities
         public int Quantity { get; private set; }
         [MaxLength(500)]
         public string Description { get; private set;}
-        public int? WarehouseId { get; private set; }
+        public int WarehouseId { get; private set; }
         [ForeignKey("WarehouseId")]
         public Warehouse Warehouse { get; private set; }
         public int MasterId { get; private set; }
