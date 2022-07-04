c2af9d32ab7c37dcf49e8ba2a272b1bef392942d
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-07-04 18:41:28 +0500

    updated requisition repo

--- Infrastructure/Repositories/RequisitionRepository.cs
+++ Infrastructure/Repositories/RequisitionRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Entities;
 using Domain.Interfaces;
 using Infrastructure.Context;
+using Infrastructure.Specifications;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +12,16 @@ namespace Infrastructure.Repositories
 {
     public class RequisitionRepository : GenericRepository<RequisitionMaster, int>, IRequisitionRepository
     {
+        private readonly ApplicationDbContext _context;
         public RequisitionRepository(ApplicationDbContext context) : base(context)
         {
+            _context = context;
+        }
 
+        public IEnumerable<RequisitionLines> FindLines(ISpecification<RequisitionLines> specification)
+        {
+            return SpecificationEvaluator<RequisitionLines, int>.GetQuery(_context.RequisitionLines
+                                    .AsQueryable(), specification);
         }
     }
 }
