314443e9d160536b2e80dc44dfe4c9206f5778e4
zainnizami <zainnizami999@gmail.com>
2022-07-05 12:38:21 +0500

    Added Remarks Table in Entities

---
+++ Domain/Entities/Remark.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Base;
+using Domain.Constants;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Remark : BaseEntity<int>
+    {
+        public int DocId { get; set; }
+        public DocType DocType { get; set; }
+        [MaxLength(200)]
+        public string Remarks { get; set; }
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+}
