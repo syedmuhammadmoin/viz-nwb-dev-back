e0a247cd49b1c8e82431b66a6ee2cb46fca2e3eb
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-07-15 17:36:46 +0500

    Added Repo for Issuance to issuance return

---
+++ Infrastructure/Repositories/IssuanceToIssuanceReturnLineReconcileRepository.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Context;
+using Infrastructure.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class IssuanceToIssuanceReturnLineReconcileRepository : IIssuanceToIssuanceReturnLineReconcileRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IssuanceToIssuanceReturnLineReconcileRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IssuanceToIssuanceReturnLineReconcile> Add(IssuanceToIssuanceReturnLineReconcile entity)
+        {
+            var result = await _context.IssuanceToIssuanceReturnLineReconcile.AddAsync(entity);
+            return result.Entity;
+        }
+
+        public IEnumerable<IssuanceToIssuanceReturnLineReconcile> Find(ISpecification<IssuanceToIssuanceReturnLineReconcile> specification)
+        {
+            return SpecificationEvaluator<IssuanceToIssuanceReturnLineReconcile, int>.GetQuery(_context.IssuanceToIssuanceReturnLineReconcile
+                                     .AsQueryable(), specification);
+        }
+    }
+}
