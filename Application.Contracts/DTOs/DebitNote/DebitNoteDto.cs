40ca8fd4735500ea74e56fb68a03cf337d6c4d53
zainnizami <zainnizami999@gmail.com>
2022-07-21 12:35:23 +0500

    Added FileUPload Property in Debit note Dto

--- Application.Contracts/DTOs/DebitNote/DebitNoteDto.cs
+++ Application.Contracts/DTOs/DebitNote/DebitNoteDto.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Constants;
+﻿using Application.Contracts.DTOs.FileUpload;
+using Domain.Constants;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +32,9 @@ namespace Application.Contracts.DTOs
         public int? LedgerId { get; set; }
         public int? TransactionId { get; set; }
         public virtual List<DebitNoteLinesDto> DebitNoteLines { get; set; }
+        public IEnumerable<FileUploadDto> FileUploadList { get; set; }
+
+
         public bool IsAllowedRole { get; set; }
     }
 }
