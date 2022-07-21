16d54f801d78ca5e8a26fa53d6ca00c3b8f18346
zainnizami <zainnizami999@gmail.com>
2022-07-21 12:35:50 +0500

    Created FileUpload Dto

---
+++ Application.Contracts/DTOs/FileUpload/FileUploadDto.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs.FileUpload
+{
+    public class FileUploadDto
+    {
+        public int Id { get; set; }
+        public DocType DocType { get; set; }
+        public string Name { get; set; }
+        public string FileType { get; set; }
+        public string Extension { get; set; }
+        public string UserName { get; set; }
+        public string CreatedAt { get; set; }
+    }
+}
