627c209de929e6b8f586b1f15a3750e04d655f5b
zainnizami <zainnizami999@gmail.com>
2022-07-27 18:20:00 +0500

    Added Remarks list GoodReturnNoteDto

--- Application.Contracts/DTOs/GoodsReturnNote/GoodsReturnNoteDto.cs
+++ Application.Contracts/DTOs/GoodsReturnNote/GoodsReturnNoteDto.cs
@@ -26,6 +26,7 @@ namespace Application.Contracts.DTOs
         public decimal TotalAmount { get; set; }
         public string GRNDocNo { get; set; }
         public virtual List<GoodsReturnNoteLinesDto> GoodsReturnNoteLines { get; set; }
+        public IEnumerable <RemarksDto> RemarksList { get; set; }
         public bool IsAllowedRole { get; set; }
     }
 }
