6aa9eb12a26928e7f63eede043adf870efd27729
zainnizami <zainnizami999@gmail.com>
2022-07-28 13:14:42 +0500

    Added  Fileupload method in GRN

--- VizalysDDD/Controllers/GRNController.cs
+++ VizalysDDD/Controllers/GRNController.cs
@@ -16,10 +16,12 @@ namespace Vizalys.Api.Controllers
     public class GRNController : ControllerBase
     {
         private readonly IGRNService _gRNService;
+        private readonly IFileuploadServices _fileUploadService;
 
-        public GRNController(IGRNService gRNService)
+        public GRNController(IGRNService gRNService, IFileuploadServices fileUploadService)
         {
             _gRNService = gRNService;
+            _fileUploadService = fileUploadService; 
         }
 
         [ClaimRequirement("Permission", new string[] { Permissions.GRNClaims.Create })]
@@ -76,6 +78,27 @@ namespace Vizalys.Api.Controllers
             if (result.IsSuccess)
                 return Ok(result); // Status Code : 200
             return BadRequest(result);
+        }
+
+        [HttpPost("DocUpload/{id:int}")]
+        public async Task<ActionResult<Response<int>>> UploadFile(IFormFile file, int id)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var result = await _fileUploadService.UploadFile(file, id, DocType.GRN);
+                    if (result.IsSuccess)
+                        return Ok(result); // Status Code : 200
+                    return BadRequest(result);
+                }
+                return BadRequest("Some properties are not valid"); // Status code : 400
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    e.Message);
+            }
         }
     }
 }
