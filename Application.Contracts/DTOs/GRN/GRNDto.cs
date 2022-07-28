1803a38ef8a5a3a0f2a8db469a69e31f68c64e9e
zainnizami <zainnizami999@gmail.com>
2022-07-28 13:12:04 +0500

    Added File upload list in GRN

--- Application.Contracts/DTOs/GRN/GRNDto.cs
+++ Application.Contracts/DTOs/GRN/GRNDto.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Constants;
+﻿using Application.Contracts.DTOs.FileUpload;
+using Domain.Constants;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +30,7 @@ namespace Application.Contracts.DTOs
         public string IssuanceDocNo { get; set; }
         public IEnumerable<ReferncesDto> References { get; set; }
         public IEnumerable<RemarksDto> RemarksList { get; set; }
+        public IEnumerable<FileUploadDto> FileUploadList { get; set; }
 
         public ReferncesDto BillReference { get; set; }
         public virtual List<GRNLinesDto> GRNLines { get; set; }
