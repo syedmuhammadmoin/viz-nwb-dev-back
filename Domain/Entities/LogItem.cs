2a4c37f59710db64ee716fd64b9b88b738fd8719
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-09-14 11:47:53 +0500

    Create new Entity Logitem

---
+++ Domain/Entities/LogItem.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class LogItem : BaseEntity<Guid>
+    { 
+        
+        public string Title { get; set; }
+        public int Status { get; set; }
+        public string Detail { get; set; }
+        public string TraceId { get; set; }
+
+    }
+
+}
