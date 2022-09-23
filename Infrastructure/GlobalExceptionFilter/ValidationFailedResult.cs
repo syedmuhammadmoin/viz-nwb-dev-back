ccbc90479b3e20b8af0c991d9a3d596bab210453
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-09-23 17:57:18 +0500

    Create Validation Failed Result

---
+++ Infrastructure/GlobalExceptionFilter/ValidationFailedResult.cs
@@ -0,0 +1,26 @@
+﻿using Application.Contracts.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.GlobalExceptionFilter
+{
+    public class ValidationFailedResult : ObjectResult
+    {
+        public ValidationFailedResult(HttpContext context, ModelStateDictionary modelState)
+            : base(new Response<bool>( "Validation Failed" , 400 , context.TraceIdentifier, modelState.Keys
+                     .SelectMany(key => modelState[key].Errors.Select(x => new ValidationError(key, 0, x.ErrorMessage)))
+                     .ToList()))
+        { 
+            StatusCode = StatusCodes.Status400BadRequest; //change the http status code to 422.
+        }
+    }
+}
