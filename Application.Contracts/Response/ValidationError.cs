717be888b83fed341dcf49d85d4f17e4812703d8
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-09-23 17:55:54 +0500

    Create ValidationError  Class

---
+++ Application.Contracts/Response/ValidationError.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Response
+{
+    public class ValidationError
+    {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string Field { get; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public int Code { get; set; }
+        public string error { get; }
+
+
+        public ValidationError(string field, int code, string message)
+        {
+            Field = field != string.Empty ? field : null;
+            error = message;
+        }
+    }
+}
