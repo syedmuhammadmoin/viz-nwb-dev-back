1c90ebb9a3f0e9567a4c347ac3db4353dc23e2c5
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-09-23 17:56:48 +0500

    Create Error Handling Filter

--- Infrastructure/GlobalExceptionFilter/ErrorHandlingFilters.cs
+++ Infrastructure/GlobalExceptionFilter/ErrorHandlingFilters.cs
@@ -1,4 +1,4 @@
-﻿using Application.Contracts.DTOs;
+﻿
 using Application.Contracts.Response;
 using Domain.Entities;
 using Infrastructure.Context;
@@ -18,29 +18,22 @@ namespace Infrastructure.GlobalExceptionFilter
         public override void OnException(ExceptionContext context)
         {
             var exception = context.Exception;
-            var StatusCode = StatusCodes.Status500InternalServerError;
-            string id = context.HttpContext.TraceIdentifier;
-            var StackTrace = exception.StackTrace;
-            var result = new Response<LogItemDto>("Something went wrong");
-            var IsSuccess = result.IsSuccess;
-            var Errors = result.Errors;
-            var res = result.Result;
-            var Message = result.Message;
-            context.Result = new ObjectResult(new { Message = Message, TraceId = id, StasCode = StatusCode, Result = res, IsSuccess = IsSuccess, Errors = Errors })
+            var responses = new Response<bool>("Something went wrong", StatusCodes.Status500InternalServerError, context.HttpContext.TraceIdentifier);
+            context.Result = new ObjectResult(responses)
             {
                 StatusCode = 500
             };
-            var Log = new LogItem
+            var Logs = new LogItem
             {
-                Status = StatusCode,
-                Title = "Something Went Wrong",
-                Detail = StackTrace,
-                TraceId = id
+                Status = responses.StatusCode,
+                Message = exception.Message,
+                Detail = exception.StackTrace,
+                TraceId = context.HttpContext.TraceIdentifier
             };
-            _dbContext.LogItems.Add(Log);
+            _dbContext.LogItems.Add(Logs);
             _dbContext.SaveChanges();
             context.ExceptionHandled = true;
-
+      
         }
 
     }
