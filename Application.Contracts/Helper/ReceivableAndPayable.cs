49f58fdc67cc181b826b9bc678476115794da7b0
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-11-03 15:46:12 +0500

    Add  Payable and Receivable Class for Validation

---
+++ Application.Contracts/Helper/ReceivableAndPayable.cs
@@ -0,0 +1,75 @@
+﻿using Application.Contracts.Response;
+using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Helper
+{
+    public static class ReceivableAndPayable
+    {
+    
+        public static bool Validate(Guid id)
+        {
+            if (id == new Guid("12200000-5566-7788-99AA-BBCCDDEEFF00")
+                               || id == new Guid("12100000-5566-7788-99AA-BBCCDDEEFF00")
+                               || id == new Guid("12300000-5566-7788-99AA-BBCCDDEEFF00")
+                               || id == new Guid("12900000-5566-7788-99AA-BBCCDDEEFF00")
+                               || id == new Guid("12110000-5566-7788-99AA-BBCCDDEEFF00")
+                               || id == new Guid("12120000-5566-7788-99AA-BBCCDDEEFF00")
+                               || id == new Guid("22100000-5566-7788-99AA-BBCCDDEEFF00")
+                               )
+            {
+                return false; // Account Invalid
+            }
+            else
+            {
+                return true; // Account valid
+            }
+        }
+
+        public static bool ValidateBankAccount(Guid id)
+        {
+            if( id == new Guid("12500000-5566-7788-99AA-BBCCDDEEFF00"))
+            {
+                return true; // Account Valid
+            }
+            else
+            {
+                return false; // Account Invalid
+            }
+        }
+        public static bool ValidateReceivable(Guid id)
+        {
+            if( 
+             id == new Guid("12200000-5566-7788-99AA-BBCCDDEEFF00")
+                 || id == new Guid("12100000-5566-7788-99AA-BBCCDDEEFF00")
+                 || id == new Guid("12300000-5566-7788-99AA-BBCCDDEEFF00")
+                 || id == new Guid("12900000-5566-7788-99AA-BBCCDDEEFF00")
+                 || id == new Guid("12110000-5566-7788-99AA-BBCCDDEEFF00")
+                 || id == new Guid("12120000-5566-7788-99AA-BBCCDDEEFF00"))
+            {
+                return true; // Account Receivable
+            }
+            else
+            {
+                return false; // Account Invalid
+            }
+        }
+        public static bool ValidatePayable(Guid id)
+        {
+            if (
+             id == new Guid("22100000-5566-7788-99AA-BBCCDDEEFF00"))
+             {
+                return true; // Account Payable
+            }
+            else
+            {
+                return false;// Account Invalid
+            }
+        }
+    }
+}
