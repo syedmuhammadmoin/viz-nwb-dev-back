e7f263c3ab6ff5422045103bc9f29b43c50f05e3
Waleed Tariq <dev.ratech@outlook.com>
2022-11-11 20:05:02 +0500

    add employee fields

--- Domain/Entities/PayrollTransactionMaster.cs
+++ Domain/Entities/PayrollTransactionMaster.cs
@@ -15,21 +15,12 @@ namespace Domain.Entities
         public string DocNo { get;private set; }
         public int Month { get;private set; }
         public int Year { get;private set; }
-        public int EmployeeId { get;private set; }
-        [ForeignKey("EmployeeId")]
-        public Employee Employee { get;private set; }
         public Guid BPSAccountId { get; private set; }
         [MaxLength(500)]
         public string BPSName { get; private set; }
         public int CampusId { get;private set; }
         [ForeignKey("CampusId")]
         public Campus Campus { get;private set; }
-        public int DesignationId { get;private set; }
-        [ForeignKey("DesignationId")]
-        public Designation Designation { get;private set; }
-        public int DepartmentId { get;private set; }
-        [ForeignKey("DepartmentId")]
-        public Department Department { get;private set; }
         public Guid? AccountPayableId { get;private set; }
         [ForeignKey("AccountPayableId")]
         public Level4 AccountPayable { get;private set; }
@@ -43,10 +34,6 @@ namespace Domain.Entities
         public decimal GrossSalary { get;private set; }
         [Column(TypeName = "decimal(18,2)")]
         public decimal NetSalary { get;private set; }
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal NetIncrement { get; private set; }
-        [MaxLength(50)]
-        public string EmployeeType { get; private set; }
         public int StatusId { get;private set; }
         [ForeignKey("StatusId")]
         public WorkFlowStatus Status { get;private set; }
@@ -54,17 +41,121 @@ namespace Domain.Entities
         [ForeignKey("TransactionId")]
         public Transactions Transactions { get;private set; }
         public int? LedgerId { get; private set; }
+
+        /// <summary>
+        /// Employee Fields
+        /// </summary>
+        public int EmployeeId { get; private set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; private set; }
+
+        [MaxLength(100)]
+        public string Name { get; private set; }
+        [MaxLength(100)]
+        public string FatherName { get; private set; }
+        [MaxLength(20)]
+        public string CNIC { get; private set; }
+        [MaxLength(40)]
+        public string EmployeeType { get; private set; } 
+        [MaxLength(50)]
+        public string BankName { get; private set; }
+        [MaxLength(50)]
+        public string BranchName { get; private set; }
+        [MaxLength(50)]
+        public string AccountTitle { get; private set; }
+        [MaxLength(50)]
+        public string AccountNumber { get; private set; }
+        [MaxLength(30)]
+        public string EmployeeCode { get; private set; }
+        [MaxLength(100)]
+        public string Domicile { get; private set; }
+        [MaxLength(20)]
+        public string Contact { get; private set; }
+        [MaxLength(20)]
+        public string Religion { get; private set; }
+        [MaxLength(50)]
+        public string Nationality { get; private set; }
+        [MaxLength(50)]
+        public string Maritalstatus { get; private set; }
+        [MaxLength(20)]
+        public string Gender { get; private set; }
+        [MaxLength(50)]
+        public string PlaceofBirth { get; private set; }
+        public int DesignationId { get; private set; }
+        [ForeignKey("DesignationId")] 
+        public Designation Designation { get; private set; }
+        public int DepartmentId { get; private set; }
+        [ForeignKey("DepartmentId")]
+        public Department Department { get; private set; }
+        [MaxLength(300)]
+        public string Address { get; private set; }
+        public DateTime DateofJoining { get; private set; }
+        public DateTime DateofRetirment { get; private set; }
+        public DateTime DateofBirth { get; private set; }
+        public int EarnedLeaves { get; private set; }
+        public int CasualLeaves { get; private set; }
+        [MaxLength(80)]
+        public string Faculty { get; private set; }
+        [MaxLength(80)]
+        public string DutyShift { get; private set; }
+        public int? NoOfIncrements { get; private set; }
+        [MaxLength(100)]
+        public string Email { get; private set; }
+
+        /// <summary>
+        /// Payroll Items for Basic Pay and Increment
+        /// </summary>
+        public int BasicPayItemId { get; private set; }
+        [ForeignKey("BasicPayItemId ")]
+        public PayrollItem BasicPayItem { get; private set; }
+        public int? IncrementItemId { get; private set; }
+        [ForeignKey("IncrementItemId")]
+        public PayrollItem IncrementItem { get; private set; }
+
+        // Payroll Lines
         public virtual List<PayrollTransactionLines> PayrollTransactionLines { get;private set; }
 
-        public PayrollTransactionMaster(int month, int year, int employeeId, Guid bpsAccountId, string bpsName, int designationId, int departmentId, int campusId, int workingDays, int presentDays, int leaveDays, DateTime transDate, decimal basicSalary, decimal grossSalary, decimal netIncrement, decimal netSalary, int statusId, string employeeType, List<PayrollTransactionLines> payrollTransactionLines)
+
+
+        //Construtors
+        //public PayrollTransactionMaster(int month, int year, int employeeId, Guid bpsAccountId, string bpsName, int designationId, int departmentId, int campusId, int workingDays, int presentDays, int leaveDays, DateTime transDate, decimal basicSalary, decimal grossSalary, decimal netIncrement, decimal netSalary, int statusId, string employeeType, List<PayrollTransactionLines> payrollTransactionLines)
+        //{
+        //    Month = month;
+        //    Year = year;
+        //    EmployeeId = employeeId;
+        //    BPSAccountId = bpsAccountId;
+        //    BPSName = bpsName;
+        //    DesignationId = designationId;
+        //    DepartmentId = departmentId;
+        //    CampusId = campusId;
+        //    WorkingDays = workingDays;
+        //    PresentDays = presentDays;
+        //    LeaveDays = leaveDays;
+        //    TransDate = transDate;
+        //    BasicSalary = basicSalary;
+        //    GrossSalary = grossSalary;
+        //    NetSalary = netSalary;
+        //    NetIncrement = netIncrement;
+        //    StatusId = statusId;
+        //    EmployeeType = employeeType;
+        //    PayrollTransactionLines = payrollTransactionLines;
+        //}
+
+        public PayrollTransactionMaster(int month, int year, Guid bPSAccountId, string bPSName,
+            int campusId, int workingDays, int presentDays, int leaveDays,
+            DateTime transDate, decimal basicSalary, decimal grossSalary, decimal netSalary,
+            int statusId, int employeeId, string name, string fatherName, string cNIC, string employeeType,
+            string bankName, string branchName, string accountTitle, string accountNumber, string employeeCode,
+            string domicile, string contact, string religion, string nationality, string maritalstatus, string gender,
+            string placeofBirth, int designationId, int departmentId, string address, DateTime dateofJoining,
+            DateTime dateofRetirment, DateTime dateofBirth, string faculty,
+            string dutyShift, int? noOfIncrements, string email, int basicPayItemId, int? incrementItemId,
+            List<PayrollTransactionLines> payrollTransactionLines)
         {
             Month = month;
             Year = year;
-            EmployeeId = employeeId;
-            BPSAccountId = bpsAccountId;
-            BPSName = bpsName;
-            DesignationId = designationId;
-            DepartmentId = departmentId;
+            BPSAccountId = bPSAccountId;
+            BPSName = bPSName;
             CampusId = campusId;
             WorkingDays = workingDays;
             PresentDays = presentDays;
@@ -73,19 +164,74 @@ namespace Domain.Entities
             BasicSalary = basicSalary;
             GrossSalary = grossSalary;
             NetSalary = netSalary;
-            NetIncrement = netIncrement;
             StatusId = statusId;
+            EmployeeId = employeeId;
+            Name = name;
+            FatherName = fatherName;
+            CNIC = cNIC;
             EmployeeType = employeeType;
+            BankName = bankName;
+            BranchName = branchName;
+            AccountTitle = accountTitle;
+            AccountNumber = accountNumber;
+            EmployeeCode = employeeCode;
+            Domicile = domicile;
+            Contact = contact;
+            Religion = religion;
+            Nationality = nationality;
+            Maritalstatus = maritalstatus;
+            Gender = gender;
+            PlaceofBirth = placeofBirth;
+            DesignationId = designationId;
+            DepartmentId = departmentId;
+            Address = address;
+            DateofJoining = dateofJoining;
+            DateofRetirment = dateofRetirment;
+            DateofBirth = dateofBirth;
+            Faculty = faculty;
+            DutyShift = dutyShift;
+            NoOfIncrements = noOfIncrements;
+            Email = email;
+            BasicPayItemId = basicPayItemId;
+            IncrementItemId = incrementItemId;
             PayrollTransactionLines = payrollTransactionLines;
         }
 
 
-        public void updatePayrollTransaction(Guid bpsAccountId, string bpsName, int designationId, int departmentId, int workingDays, int presentDays, int leaveDays, DateTime transDate, decimal basicSalary, decimal grossSalary, decimal netSalary, decimal netIncrement, string employeeType, List<PayrollTransactionLines> payrollTransactionLines)
+
+        //public void updatePayrollTransaction(Guid bpsAccountId, string bpsName, int designationId, int departmentId, int workingDays, int presentDays, int leaveDays, DateTime transDate, decimal basicSalary, decimal grossSalary, decimal netSalary, string employeeType, List<PayrollTransactionLines> payrollTransactionLines)
+        //{
+        //    BPSAccountId = bpsAccountId;
+        //    BPSName = bpsName;
+        //    DesignationId = designationId;
+        //    DepartmentId = departmentId;
+        //    WorkingDays = workingDays;
+        //    PresentDays = presentDays;
+        //    LeaveDays = leaveDays;
+        //    TransDate = transDate;
+        //    BasicSalary = basicSalary;
+        //    GrossSalary = grossSalary;
+        //    NetSalary = netSalary;
+        //    EmployeeType = employeeType;
+        //    PayrollTransactionLines = payrollTransactionLines;
+        //}
+
+
+
+        public void updatePayrollTransaction(Guid bPSAccountId, string bPSName,
+            int campusId, int workingDays, int presentDays, int leaveDays,
+            DateTime transDate, decimal basicSalary, decimal grossSalary, decimal netSalary, int statusId,
+            string name, string fatherName, string employeeType,
+            string bankName, string branchName, string accountTitle, string accountNumber, string employeeCode,
+            string domicile, string contact, string religion, string nationality, string maritalstatus, string gender,
+            string placeofBirth, int designationId, int departmentId, string address, DateTime dateofJoining,
+            DateTime dateofRetirment, DateTime dateofBirth, string faculty,
+            string dutyShift, int? noOfIncrements, string email, int basicPayItemId, int? incrementItemId,
+            List<PayrollTransactionLines> payrollTransactionLines)
         {
-            BPSAccountId = bpsAccountId;
-            BPSName = bpsName;
-            DesignationId = designationId;
-            DepartmentId = departmentId;
+            BPSAccountId = bPSAccountId;
+            BPSName = bPSName;
+            CampusId = campusId;
             WorkingDays = workingDays;
             PresentDays = presentDays;
             LeaveDays = leaveDays;
@@ -93,10 +239,37 @@ namespace Domain.Entities
             BasicSalary = basicSalary;
             GrossSalary = grossSalary;
             NetSalary = netSalary;
-            NetIncrement = netIncrement;
+            StatusId = statusId;
+            Name = name;
+            FatherName = fatherName;
             EmployeeType = employeeType;
+            BankName = bankName;
+            BranchName = branchName;
+            AccountTitle = accountTitle;
+            AccountNumber = accountNumber;
+            EmployeeCode = employeeCode;
+            Domicile = domicile;
+            Contact = contact;
+            Religion = religion;
+            Nationality = nationality;
+            Maritalstatus = maritalstatus;
+            Gender = gender;
+            PlaceofBirth = placeofBirth;
+            DesignationId = designationId;
+            DepartmentId = departmentId;
+            Address = address;
+            DateofJoining = dateofJoining;
+            DateofRetirment = dateofRetirment;
+            DateofBirth = dateofBirth;
+            Faculty = faculty;
+            DutyShift = dutyShift;
+            NoOfIncrements = noOfIncrements;
+            Email = email;
+            BasicPayItemId = basicPayItemId;
+            IncrementItemId = incrementItemId;
             PayrollTransactionLines = payrollTransactionLines;
         }
+
         public void updateAccountPayableId(Guid accountPayableId,int statusId)
         {
             AccountPayableId = accountPayableId;
---
+++ Infrastructure/Migrations/20221111143223_AddAllEmpFieldsInPayroll.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddAllEmpFieldsInPayroll : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NetIncrement",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AccountNumber",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AccountTitle",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BankName",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BasicPayItemId",
+                table: "PayrollTransactionMaster",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BranchName",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CNIC",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CasualLeaves",
+                table: "PayrollTransactionMaster",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Contact",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateofBirth",
+                table: "PayrollTransactionMaster",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateofJoining",
+                table: "PayrollTransactionMaster",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateofRetirment",
+                table: "PayrollTransactionMaster",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Domicile",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DutyShift",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(80)",
+                maxLength: 80,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EarnedLeaves",
+                table: "PayrollTransactionMaster",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeCode",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeType",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Faculty",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(80)",
+                maxLength: 80,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FatherName",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "IncrementItemId",
+                table: "PayrollTransactionMaster",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Maritalstatus",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nationality",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NoOfIncrements",
+                table: "PayrollTransactionMaster",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PlaceofBirth",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Religion",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PayrollTransactionMaster_BasicPayItemId",
+                table: "PayrollTransactionMaster",
+                column: "BasicPayItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PayrollTransactionMaster_IncrementItemId",
+                table: "PayrollTransactionMaster",
+                column: "IncrementItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PayrollTransactionMaster_PayrollItems_BasicPayItemId",
+                table: "PayrollTransactionMaster",
+                column: "BasicPayItemId",
+                principalTable: "PayrollItems",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PayrollTransactionMaster_PayrollItems_IncrementItemId",
+                table: "PayrollTransactionMaster",
+                column: "IncrementItemId",
+                principalTable: "PayrollItems",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PayrollTransactionMaster_PayrollItems_BasicPayItemId",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PayrollTransactionMaster_PayrollItems_IncrementItemId",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PayrollTransactionMaster_BasicPayItemId",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PayrollTransactionMaster_IncrementItemId",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "AccountNumber",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "AccountTitle",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "BankName",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "BasicPayItemId",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "BranchName",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "CNIC",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "CasualLeaves",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "Contact",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "DateofBirth",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "DateofJoining",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "DateofRetirment",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "Domicile",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "DutyShift",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "EarnedLeaves",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeCode",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeType",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "Faculty",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "FatherName",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "IncrementItemId",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "Maritalstatus",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "Nationality",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "NoOfIncrements",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "PlaceofBirth",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.DropColumn(
+                name: "Religion",
+                table: "PayrollTransactionMaster");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "NetIncrement",
+                table: "PayrollTransactionMaster",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
