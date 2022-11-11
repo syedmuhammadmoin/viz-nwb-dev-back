50d4c0205491d961ff56f6cfad67e042eddf2ee6
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-11-11 11:15:36 +0500

    Added Migration for adding employeeType in PayrollTransactionMaster

---
+++ Infrastructure/Migrations/20221110135150_AddEmployeeTypeInPayrollMaster.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddEmployeeTypeInPayrollMaster : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeType",
+                table: "PayrollTransactionMaster",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmployeeType",
+                table: "PayrollTransactionMaster");
+        }
+    }
+}
