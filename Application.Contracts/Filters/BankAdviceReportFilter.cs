53ce7c50a01480449f99ae6cab3d943614190a1f
ashir.azeem <ashir.azeem@ratechnologies.biz>
2022-11-15 17:14:42 +0500

    Added BankAdviceReportFilter

---
+++ Application.Contracts/Filters/BankAdviceReportFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Filters
+{
+    public class BankAdviceReportFilter
+    {
+        [Required]
+        public int? CampusId { get; set; }
+        [Required]
+        public int? Month { get; set; }
+        [Required]
+        public int? Year { get; set; }
+    }
+}
