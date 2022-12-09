30b84adf7890b6b0a4920938bd7937b3554755a9
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-12-09 19:17:21 +0500

    Add entity for request form

---
+++ Domain/Entities/RequestLines.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class RequestLines : BaseEntity<int>
+    {
+        public string ItemDescription { get; private set; }
+        public int ItemQuantity { get; private set; }
+        public int MasterId { get; private set; }
+        [ForeignKey("MasterId")]
+        public RequestMaster RequestMaster { get; private set; }
+        protected RequestLines()
+        {
+        }
+    }
+}
