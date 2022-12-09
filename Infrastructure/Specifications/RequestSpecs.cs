6e5c990d5e44421fa94edd44d7385e4e1afc8d9c
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-12-09 19:19:01 +0500

    Add specs for Request form

---
+++ Infrastructure/Specifications/RequestSpecs.cs
@@ -0,0 +1,51 @@
+﻿using Application.Contracts.Filters;
+using Domain.Constants;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Specifications
+{
+    public class RequestSpecs : BaseSpecification<RequestMaster>
+    {
+        public RequestSpecs(List<DateTime?> docDate,
+            List<DocumentStatus?> states, TransactionFormFilter filter, bool isTotalRecord)
+            : base(x => (docDate.Count() > 0 ? docDate.Contains(x.RequestDate) : true)
+               && x.DocNo.Contains(filter.DocNo != null ? filter.DocNo : "")
+            && x.Employee.Name.Contains(filter.BusinessPartner != null ? filter.BusinessPartner : "")
+            && (states.Count() > 0 ? states.Contains(x.Status.State) : true))
+        {
+            if (!isTotalRecord)
+            {
+                var validFilter = new PaginationFilter(filter.PageStart, filter.PageEnd);
+                ApplyPaging(validFilter.PageStart, validFilter.PageEnd - validFilter.PageStart);
+                ApplyOrderByDescending(i => i.Id);
+                AddInclude(i => i.Campus);
+                AddInclude(i => i.Status);
+                AddInclude(i => i.Employee);
+            }
+        }
+        public RequestSpecs(bool forEdit)
+        {
+            if (forEdit)
+            {
+                AddInclude(i => i.RequestLines);
+                AddInclude(i => i.Status);
+            }
+            else
+            {
+                AddInclude(i => i.Campus);
+                AddInclude(i => i.Status);
+                AddInclude(i => i.Employee);
+            }
+        }
+
+        public RequestSpecs() : base(x => x.Status.State != DocumentStatus.Paid)
+        {
+            AddInclude(i => i.RequestLines);
+        }
+    }
+}
