c8bfd773394b05a77d9747650797d27fa8e21efb
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-12-09 19:18:34 +0500

    Add request repository

---
+++ Infrastructure/Repositories/RequestRepository.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Context;
+using Infrastructure.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class RequestRepository : GenericRepository<RequestMaster, int>, IRequestRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public RequestRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<RequestLines> FindLines(ISpecification<RequestLines> specification)
+        {
+            return SpecificationEvaluator<RequestLines, int>.GetQuery(_context.RequestFormLines
+                .AsQueryable(), specification);
+        }
+    }
+}
