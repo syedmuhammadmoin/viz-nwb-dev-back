20f51b13f9c30e61ca4226a435058954c376f356
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-12-13 17:41:05 +0500

    Add workflow api

--- VizalysDDD/Controllers/RequestController.cs
+++ VizalysDDD/Controllers/RequestController.cs
@@ -64,6 +64,15 @@ namespace Vizalys.Api.Controllers
             return BadRequest(result); // Status code : 400
         }
 
+        [HttpPost("workflow")]
+        public async Task<ActionResult<Response<bool>>> CheckWorkFlow([FromBody] ApprovalDto data)
+        {
+            var result = await _requestService.CheckWorkFlow(data);
+            if (result.IsSuccess)
+                return Ok(result); // Status Code : 200
+            return BadRequest(result);
+        }
+
 
     }
 }
