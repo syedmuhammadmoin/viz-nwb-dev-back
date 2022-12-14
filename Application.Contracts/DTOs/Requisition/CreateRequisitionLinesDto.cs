f72ceaac91619398f9fcf344b28fc67fe3daaa08
Taha Israr <Taha.israr@ratechnologies.net>
2022-12-14 16:56:20 +0500

    update CreateRequisitionLinesDto

--- Application.Contracts/DTOs/Requisition/CreateRequisitionLinesDto.cs
+++ Application.Contracts/DTOs/Requisition/CreateRequisitionLinesDto.cs
@@ -13,7 +13,7 @@ namespace Application.Contracts.DTOs
         [Required]
         public int? ItemId { get; set; }
         [Required]
-        public decimal PurchasePrice { get; private set; }
+        public decimal PurchasePrice { get;  set; }
 
         [Required]
         [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than 0")]
