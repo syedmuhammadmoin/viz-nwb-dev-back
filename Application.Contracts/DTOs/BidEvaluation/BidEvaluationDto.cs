30a64b720fe8e323aa62041b5e368379a1199e09
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-12-14 18:20:38 +0500

    Create Dtos for BidEvaluation Dtos

---
+++ Application.Contracts/DTOs/BidEvaluation/BidEvaluationDto.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs.BidEvaluation
+{
+    public class BidEvaluationDto
+    {
+        public int Id { get; set; }
+        public string DocNo { get; private set; }
+        public string Name { get;  set; }
+        public string Title { get;  set; }
+        public string RefNo { get;  set; }
+        public string MethodOfProcurement { get;  set; }
+        public string TendorInquiryNumber { get;  set; }
+        public int NumberOfBids { get;  set; }
+        public DateTime DateOfOpeningBid { get;  set; }
+        public DateTime DateOfClosingBid { get;  set; }
+        public string BidEvaluationCriteria { get;  set; }
+        public string LowestEvaluatedBidder { get;  set; }
+        public DocumentStatus status { get; set; }
+        public virtual List<CreateBidEvaluationLinesDtos> BidEvaluationLinesDtos { get; set; }
+    }
+}
---
+++ Application.Contracts/DTOs/BidEvaluation/CreateBidEvaluationLinesDtos.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class CreateBidEvaluationLinesDtos
+    {
+        public int? Id { get; set; }
+        [Required]
+        public string NameOfBider { get; private set; }
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TechnicalTotal { get; private set; }
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TechnicalObtain { get; private set; }
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal FinancialTotal { get; private set; }
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal FinancialObtain { get; private set; }
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal EvaluatedCost { get; private set; }
+        [Required]
+        [MaxLength(500)]
+        public string Rule { get; private set; }
+    }
+}
