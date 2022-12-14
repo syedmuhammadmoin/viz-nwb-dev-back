17b586da5cf56dac76d220cffb1e18f2ea880eaf
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-12-14 18:22:35 +0500

    Add Doctype BidEvaluation

--- Domain/Constants/Enums.cs
+++ Domain/Constants/Enums.cs
@@ -73,6 +73,7 @@ namespace Domain.Constants
         GoodsReturnNote = 19,
         IssuanceReturn = 20,
         Request = 21,
+        BidEvaluation = 22
     }
     public enum ActionButton
     {
