4507b887b9b1ced70d52b05b6d5b1397482c9c73
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-12-14 18:26:54 +0500

    Add permission to user of BidEvaluation

--- Infrastructure/Seeds/DefaultUsers.cs
+++ Infrastructure/Seeds/DefaultUsers.cs
@@ -77,6 +77,7 @@ namespace Infrastructure.Seeds
             await roleManager.AddPermissionClaim(superAdmin, "Procurement", "GoodsReturnNoteClaims");
             await roleManager.AddPermissionClaim(superAdmin, "Procurement", "IssuanceReturnClaims");
             await roleManager.AddPermissionClaim(superAdmin, "Procurement", "RequestClaims");
+            await roleManager.AddPermissionClaim(superAdmin, "Procurement", "BidEvaluationClaims");
             await roleManager.AddPermissionClaimReport(superAdmin, "Finance", "ChartOfAccountClaims");
             await roleManager.AddPermissionClaimReport(superAdmin, "Report", "GeneralLedgerClaims");
             await roleManager.AddPermissionClaimReport(superAdmin, "Report", "TrialBalanceClaims");
