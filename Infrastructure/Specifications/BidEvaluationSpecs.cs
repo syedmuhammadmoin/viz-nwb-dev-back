a2e5103a8619fbccbaf10c580428f06e133afaad
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-12-14 18:27:12 +0500

    Create BidEvaluation Specs

---
+++ Infrastructure/Specifications/BidEvaluationSpecs.cs
@@ -0,0 +1,31 @@
+﻿using Application.Contracts.Filters;
+using Domain.Constants;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Specifications
+{
+    public class BidEvaluationSpecs : BaseSpecification<BidEvaluationMaster>
+    {
+        public BidEvaluationSpecs(List<DateTime?> OpeningDate, List<DateTime?> CloseingDate,
+            List<DocumentStatus?> states, TransactionFormFilter filter, bool isTotalRecord)
+        {
+        }
+
+        public BidEvaluationSpecs(bool forEdit)
+        {
+            if (forEdit)
+            {
+                AddInclude(i => i.BidEvaluationLines);
+            }
+            else
+            {
+           
+            }
+        }
+    }
+}
