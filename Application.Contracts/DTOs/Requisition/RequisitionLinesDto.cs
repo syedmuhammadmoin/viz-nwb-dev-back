fa0f6733dac64176c9fc6967fc464d84b88ae223
Taha Israr <Taha.israr@ratechnologies.net>
2022-12-14 20:02:00 +0500

    add SubTotal in Requisition

--- Application.Contracts/DTOs/Requisition/RequisitionLinesDto.cs
+++ Application.Contracts/DTOs/Requisition/RequisitionLinesDto.cs
@@ -15,6 +15,7 @@ namespace Application.Contracts.DTOs
         public string Description { get; set; }
         public int Quantity { get; set; }
         public decimal PurchasePrice { get; set; }
+        public decimal SubTotal { get; set; }
         public int IssuedQuantity { get; set; }
         public int PendingQuantity { get; set; }
         public int AvailableQuantity { get; set; }
