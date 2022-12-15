6c25a30a38d623f09806651b7f7a6acc47b941d7
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-12-15 16:01:34 +0500

    Update BidEvaluation Form

--- Infrastructure/Specifications/BidEvaluationSpecs.cs
+++ Infrastructure/Specifications/BidEvaluationSpecs.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Contracts.Filters;
 using Domain.Constants;
 using Domain.Entities;
+using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +12,20 @@ namespace Infrastructure.Specifications
 {
     public class BidEvaluationSpecs : BaseSpecification<BidEvaluationMaster>
     {
-        public BidEvaluationSpecs(List<DateTime?> OpeningDate, List<DateTime?> CloseingDate,
-            List<DocumentStatus?> states, TransactionFormFilter filter, bool isTotalRecord)
-        {
-        }
+        public BidEvaluationSpecs(List<DateTime?> OpeningDate,
+        List<DocumentStatus?> states, TransactionFormFilter filter, bool isTotalRecord) : base(x => (OpeningDate.Count() > 0 ? OpeningDate.Contains(x.DateOfOpeningBid) : true)
 
+         && x.DocNo.Contains(filter.DocNo != null ? filter.DocNo : "")
+        && states.Count() > 0 ? states.Contains(x.State) : true)
+        {
+            if (!isTotalRecord)
+            {
+                var validFilter = new PaginationFilter(filter.PageStart, filter.PageEnd);
+                ApplyPaging(validFilter.PageStart, validFilter.PageEnd - validFilter.PageStart);
+                ApplyOrderByDescending(i => i.Id);
+
+            }
+        }
         public BidEvaluationSpecs(bool forEdit)
         {
             if (forEdit)
@@ -24,7 +34,7 @@ namespace Infrastructure.Specifications
             }
             else
             {
-           
+                AddInclude(i => i.BidEvaluationLines);
             }
         }
     }
