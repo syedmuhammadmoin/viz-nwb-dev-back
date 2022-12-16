a597187f57f7744cb8edf7696deaddbb33ef3ddf
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-12-16 18:50:16 +0500

    Add table for Quotation

---
+++ Domain/Entities/QuotationMaster.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class QuotationMaster : BaseEntity<int>
+    {
+        public int VendorId { get; private set; }
+        [ForeignKey("VendorId")]
+        public BusinessPartner Vendor { get; private set; }
+        [MaxLength(100)]
+        public string Timeframe { get; private set; }
+        [MaxLength(30)]
+        public string DocNo { get; private set; }
+        public DateTime QuotationDate  { get; private set; }
+        public int StatusId { get; private set; }
+        [ForeignKey("StatusId")]
+        public WorkFlowStatus Status { get; private set; }
+        public int? RequisitionId { get; private set; }
+        public virtual List<QuotationLines> QuotationLines { get; private set; }
+
+        protected QuotationMaster()
+        {
+        }
+        public void setStatus(int statusId)
+        {
+            StatusId = statusId;
+        }
+
+        public void CreateDocNo()
+        {
+           DocNo = "QUOT-" + String.Format("{0:000}", Id);
+        }
+    }
+}
