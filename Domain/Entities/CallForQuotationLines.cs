80ba5b18288ac461b68c34cac1d1548cf0f6773f
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-12-19 15:42:50 +0500

    Create Table For CallForQuotation

---
+++ Domain/Entities/CallForQuotationLines.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class CallForQuotationLines : BaseEntity<int>
+    {
+        public int? ItemId { get; private set; }
+        [ForeignKey("ItemId")]
+        public Product Item { get; private set; }
+        [MaxLength(500)]
+        public string Description { get; private set; }
+        public int Quantity { get; private set; }
+        public int MasterId { get; private set; }
+        [ForeignKey("MasterId")]
+        public CallForQuotationMaster CallForQuotation { get; private set; }
+
+        protected CallForQuotationLines()
+        {
+        }
+    }
+}
