e800966f3ff875075691a067d42acb69f531612d
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-12-19 15:39:19 +0500

    Create Dtos for CallForQuotation

---
+++ Application.Contracts/DTOs/CallForQuotation/CallForQuotationDto.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class CallForQuotationDto
+    {
+        public int Id { get; set; }
+        public string VendorName { get; set; }
+        public string DocNo { get; set; }
+        public DateTime CallForQuotationDate { get; set; }
+        public string Status { get; set; }
+        public DocumentStatus State { get; set; }
+        public bool IsAllowedRole { get; set; }
+        public virtual List<CallForQuotationLinesDto> CallForQuotationLines { get; set; }
+
+    }
+}
---
+++ Application.Contracts/DTOs/CallForQuotation/CreateCallForQuotationLinesDto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class CreateCallForQuotationLinesDto
+    {
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than 0")]
+        public int Quantity { get; set; }
+        [MaxLength(500)]
+        public string Description { get; set; }
+    }
+}
