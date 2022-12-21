e9d7ee5207cb4802b0172c2793abfba90217ee1c
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-12-21 14:48:00 +0500

    Add New Quotation Controller

---
+++ VizalysDDD/Controllers/QuotationComparativeController.cs
@@ -0,0 +1,33 @@
+﻿using Application.Contracts.DTOs;
+using Application.Contracts.Helper;
+using Application.Contracts.Interfaces;
+using Application.Contracts.Response;
+using Domain.Constants;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vizalys.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotationComparativeController : ControllerBase
+    {
+        private readonly IQuotationComparativeService _quotationComparativeService;
+
+        public QuotationComparativeController(IQuotationComparativeService quotationComparativeService)
+        {
+            _quotationComparativeService = quotationComparativeService;
+        }
+
+        [ClaimRequirement("Permission", new string[] { Permissions.QuotationClaims.Create, Permissions.QuotationClaims.View, Permissions.QuotationClaims.Delete, Permissions.QuotationClaims.Edit })]
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Response<QuotationDto>>> GetByIdAsync(int id)
+        {
+            var result =  _quotationComparativeService.GetRequisitionById(id);
+            if (result.IsSuccess)
+                return Ok(result); // Status Code : 200
+
+            return BadRequest(result); // Status code : 400
+        }
+    }
+}
