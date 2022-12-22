d795d0ff5501bb374bee5102e8a2caca88e086ab
zainnizami <Zain.Nizami@Ratechnologies.net>
2022-12-22 17:17:05 +0500

    Addd RequisitionByid method

--- Application.Contracts/Interfaces/IQuotationService.cs
+++ Application.Contracts/Interfaces/IQuotationService.cs
@@ -11,6 +11,7 @@ namespace Application.Contracts.Interfaces
 {
     public interface IQuotationService : ICrudService<CreateQuotationDto, QuotationDto, int, TransactionFormFilter>
     {
+        Task<Response<List<QuotationDto>>> GetRequisitionById(int requisition);
         Task<Response<bool>> CheckWorkFlow(ApprovalDto data);
     }
 }
