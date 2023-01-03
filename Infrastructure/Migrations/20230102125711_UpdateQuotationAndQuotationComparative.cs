d6a4ac9657a4c2bf068e9f4e00becad6ce1c0b83
zainnizami <Zain.Nizami@Ratechnologies.net>
2023-01-03 12:55:59 +0500

    Migration for Awarded vendor

---
+++ Infrastructure/Migrations/20230102125711_UpdateQuotationAndQuotationComparative.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class UpdateQuotationAndQuotationComparative : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AwardedVendor",
+                table: "QuotationMaster",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AwardedVendor",
+                table: "QuotationComparativeMaster",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AwardedVendor",
+                table: "QuotationMaster");
+
+            migrationBuilder.DropColumn(
+                name: "AwardedVendor",
+                table: "QuotationComparativeMaster");
+        }
+    }
+}
