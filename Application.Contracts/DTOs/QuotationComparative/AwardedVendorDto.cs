a725ba0e99167c9db733085d2428626c198255c7
zainnizami <Zain.Nizami@Ratechnologies.net>
2023-01-03 10:10:41 +0500

    Add new Dto Awarded Vendor

---
+++ Application.Contracts/DTOs/QuotationComparative/AwardedVendorDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class AwardedVendorDto
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Remarks { get; set; }
+        [Required]
+        public string  AwardedVendor { get; set; }
+        [Required]
+        public int QuotationId { get; set; }
+        public bool checkBoxSelection { get; set; } = true;
+    }
+}
