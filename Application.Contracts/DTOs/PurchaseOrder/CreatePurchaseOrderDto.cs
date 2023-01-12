ce3cafd0a3a0ee53026ba70a8ac4d7bdc3cf1ef5
zainnizami <Zain.Nizami@Ratechnologies.net>
2023-01-12 12:18:32 +0500

    allow nullable

--- Application.Contracts/DTOs/PurchaseOrder/CreatePurchaseOrderDto.cs
+++ Application.Contracts/DTOs/PurchaseOrder/CreatePurchaseOrderDto.cs
@@ -20,10 +20,11 @@ namespace Application.Contracts.DTOs
         public string Contact { get; set; }
         [Required]
         public int? CampusId { get; set; }
+        public int? RequisitionId { get; private set; }
         [Required]
         public bool? isSubmit { get; set; }
         public virtual List<CreatePurchaseOrderLinesDto> PurchaseOrderLines { get; set; }
-        public int? RequisitionId { get; private set; }
+
 
     }
 }
