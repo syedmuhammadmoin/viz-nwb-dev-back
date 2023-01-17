2daa601b59131419dae94bdcf3334dbdfe6b79f2
zainnizami <Zain.Nizami@Ratechnologies.net>
2023-01-17 15:36:17 +0500

    add new specs Depreciation

---
+++ Infrastructure/Specifications/DepreciationSpecs.cs
@@ -0,0 +1,33 @@
+﻿using Application.Contracts.Filters;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Specifications
+{
+    public class DepreciationSpecs : BaseSpecification<Depreciation>
+    {
+        public DepreciationSpecs(TransactionFormFilter filter, bool isTotalRecord)
+            : base(c => c.ModelName.Contains(filter.Name != null ? filter.Name : ""))
+        {
+            if (!isTotalRecord)
+            {
+                var validFilter = new PaginationFilter(filter.PageStart, filter.PageEnd);
+                ApplyPaging(validFilter.PageStart, validFilter.PageEnd - validFilter.PageStart);
+                AddInclude(i => i.AssetAccount);
+                AddInclude(i => i.DepricationExpense);
+                AddInclude(i => i.AccumulatedDepriciation);
+                ApplyOrderByDescending(i => i.Id);
+            }
+        }
+        public DepreciationSpecs()
+        {
+            AddInclude(i => i.AssetAccount);
+            AddInclude(i => i.AccumulatedDepriciation);
+            AddInclude(i => i.DepricationExpense);
+        }
+    }
+}
