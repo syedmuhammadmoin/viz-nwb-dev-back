f8503545d019c586ad6a2c4e09481a7d840825d4
zainnizami <Zain.Nizami@Ratechnologies.net>
2023-01-17 15:31:40 +0500

    Create Dtos For Depreciation

---
+++ Application.Contracts/DTOs/Depreciation/CreateDepreciationDto.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Constants;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class CreateDepreciationDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string ModelName { get; set; }
+        [Required]
+        public int UseFullLife { get; set; }
+        [Required]
+        public Guid AssetAccountId { get; set; }
+        [Required]
+        public Guid DepricationExpenseId { get; set; }
+        [Required]
+        public Guid AccumulatedDepriciationId { get; set; }
+        [Required]
+        public DepreciationMethod ModelType { get; set; }
+        [Required]
+        [Range(0.00, 100.00, ErrorMessage = "Please enter a value between 0 and 100")]
+        public decimal? DecliningRate { get; set; }
+    }
+}
---
+++ Application.Contracts/DTOs/Depreciation/DepreciationDto.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Constants;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class DepreciationDto
+    {
+        public int Id { get; set; }
+        public string ModelName { get; set; }
+        public int UseFullLife { get; set; }
+        public Guid AssetAccountId { get; set; }
+        public string AssetAccount { get; set; }
+        public Guid DepricationExpenseId { get; set; }
+        public string DepricationExpense { get; set; }
+        public Guid AccumulatedDepriciationId { get; set; }
+        public string AccumulatedDepriciation { get; set; }
+        public string ModelType { get; set; }
+        public decimal? DecliningRate { get; set; }
+    }
+}
