d30c97d96d0b09e106ed07f2484f9901016aaf34
zainnizami <Zain.Nizami@Ratechnologies.net>
2023-01-17 15:32:48 +0500

    Create interface of  Depreciation

---
+++ Application.Contracts/Interfaces/IDepreciationService.cs
@@ -0,0 +1,16 @@
+﻿using Application.Contracts.DTOs;
+using Application.Contracts.Filters;
+using Application.Contracts.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Interfaces
+{
+    public interface IDepreciationService : ICrudService<CreateDepreciationDto, DepreciationDto, int, TransactionFormFilter>
+    {
+        Task<Response<List<DepreciationDto>>> GetDepreciationDown();
+    }
+}
