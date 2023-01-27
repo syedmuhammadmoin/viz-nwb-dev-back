3f3f2c4312d906552d96f2f0156a9edc206779fb
zainnizami <Zain.Nizami@Ratechnologies.net>
2023-01-27 11:54:51 +0500

    Create New Fixed Asset dto

---
+++ Application.Contracts/DTOs/FixedAsset/CreateFixedAssetDto.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Constants;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class CreateFixedAssetDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public DateTime DateofAcquisition { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        public int PurchaseCost { get; set; }
+        [Required]
+        public int? CategoryId { get; set; }
+        [Required]    
+        public int SalvageValue { get; set; }
+        [Required]
+        public bool DepreciationApplicability { get; set; }
+        public int? DepreciationId { get; set; }
+        [MaxLength(100)]
+        public string ModelType { get; set; }
+        public Guid? AssetAccountId { get; set; }
+        public Guid? DepreciationExpenseId { get; set; }
+        public Guid? AccumulatedDepreciationId { get; set; }
+        public int? UseFullLife { get; set; }
+        [Range(0.00, 100.00, ErrorMessage = "Please enter a value between 0 and 100")]
+        public decimal? DecLiningRate { get; set; }
+        public bool ProrataBasis { get; set; }
+        public bool Active { get;  set; }
+    }
+}
