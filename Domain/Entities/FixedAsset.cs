5d7273fc5e535b38873df330f5492337591bf0a7
zainnizami <Zain.Nizami@Ratechnologies.net>
2023-01-27 11:58:41 +0500

    Create new Entity FixedAsset

---
+++ Domain/Entities/FixedAsset.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class FixedAsset : BaseEntity<int>
+    {
+        public DateTime DateofAcquisition { get; private set; }
+        [MaxLength(100)]
+        public string Name { get; private set; }
+        public int PurchaseCost { get; private set; }
+        public int CategoryId { get; private set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; private set; }
+        public int SalvageValue { get; private set; }
+        public bool DepreciationApplicability { get; private set; }
+        public int? DepreciationId { get; private set; }
+        [ForeignKey("DepreciationId")]
+        public Depreciation Depreciation { get; private set; }
+        [MaxLength(100)]
+        public string ModelType { get; private set; }
+        public Guid? AssetAccountId { get; private set; }
+        [ForeignKey("AssetAccountId")]
+        public Level4 AssetAccount { get; private set; }
+        public Guid? DepreciationExpenseId { get; private set; }
+        [ForeignKey("DepreciationExpenseId")]
+        public Level4 DepreciationExpense { get; private set; }
+        public Guid? AccumulatedDepreciationId { get; private set; }
+        [ForeignKey("AccumulatedDepreciationId")]
+        public Level4 AccumulatedDepreciation { get; private set; }
+        public int? UseFullLife { get; private set; }
+        public decimal? DecLiningRate { get; private set; }
+        public bool ProrataBasis { get; private set; }
+        public bool Active { get; private set; }
+        protected FixedAsset()
+        {
+        }
+    }
+}
