52dcda29ff0e59421bc303f330d6b21a1b41003a
zainnizami <Zain.Nizami@Ratechnologies.net>
2023-01-27 11:55:24 +0500

    Create new Fixed Dto

---
+++ Application.Contracts/DTOs/FixedAsset/FixedAssetDto.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class FixedAssetDto
+    {
+        public int Id { get; set; }
+        public DateTime DateofAcquisition { get; set; }
+        public string Name { get; set; }
+        public int PurchaseCost { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int SalvageValue { get; set; }
+        public bool DepreciationApplicability { get; set; }
+        public int DepreciationId { get; set; }
+        public string Depreciation { get; set; }
+        public Guid AssetAccountId { get; set; }
+        public string AssetAccount { get; set; }
+        public Guid DepreciationExpenseId { get; set; }
+        public string DepreciationExpense { get; set; }
+        public Guid AccumulatedDepreciationId { get; set; }
+        public string AccumulatedDepreciation { get; set; }
+        public int UseFullLife { get; set; }
+        public decimal DecLiningRate { get; set; }
+        public bool ProrataBasis { get; set; }
+        public bool Active { get; set; }
+    }
+}
