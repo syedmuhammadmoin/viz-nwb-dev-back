34dc335fff45cc197bcc68d71218098e9983e4f6
zainnizami <Zain.Nizami@Ratechnologies.net>
2023-02-15 12:27:00 +0500

    add disposal dto

---
+++ Application.Contracts/DTOs/Disposal/CreateDisposalDto.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Constants;
+
+namespace Application.Contracts.DTOs
+{
+    public class CreateDisposalDto
+    {
+        public int? Id { get; set; }
+        [Required]
+        public int? AssetId { get; set; }
+        [Required]
+        public int? CategoryId { get; set; }
+        [Required]
+        public decimal PurchaseCost { get; set; }
+        [Required]
+        public int SalvageValue { get; set; }
+        [Required]
+        public int? UsefulLife { get; set; }
+        [Required]
+        public Guid? AccumulatedDepreciationId { get; set; }
+        [Required]
+        public decimal BookValue { get; set; }
+        [Required]
+        public DateTime DisposalDate { get; set; }
+        [Required]
+        public decimal DisposalValue { get; set; }
+        [Required]   
+        public int? WarehouseId { get; set; }
+        [Required]
+        public bool? isSubmit { get; set; }
+    }
+}
---
+++ Application.Contracts/DTOs/Disposal/DisposalDto.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class DisposalDto
+    {
+        public int Id { get; set; }
+        public string DocNo { get; set; }
+        public int AssetId { get; set; }
+        public string Asset { get; set; }
+        public int CategoryId { get; set; }
+        public string Category { get; set; }
+        public decimal PurchaseCost { get; set; }
+        public int SalvageValue { get; set; }
+        public int UsefulLife { get; set; }
+        public Guid AccumulatedDepreciationId { get; set; }
+        public string AccumulatedDepreciation { get; set; }
+        public decimal BookValue { get; set; }
+        public DateTime DisposalDate { get; set; }
+        public decimal DisposalValue { get; set; }
+        public int WarehouseId { get; set; }
+        public string Warehouse { get; set; }
+        public int StatusId { get; set; }
+        public string Status { get; set; }
+        public DocumentStatus State { get; set; }
+        public IEnumerable<RemarksDto> RemarksList { get; set; }
+        public bool IsAllowedRole { get; set; }
+    }   
+}
