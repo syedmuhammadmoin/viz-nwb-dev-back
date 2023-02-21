0e3b8f79ae819f05ac128b1e5437dc600c8ad419
zainnizami <Zain.Nizami@Ratechnologies.net>
2023-02-21 12:27:54 +0500

    Create new Dto  BudgetReappropriation

---
+++ Application.Contracts/DTOs/BudgetReappropriation/BudgetReappropriationDto.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Constants;
+
+namespace Application.Contracts.DTOs
+{
+    public class BudgetReappropriationDto
+    {
+        public int Id{ get; set; }
+        public int BudgetId { get; set; }
+        public string Budget { get; set; }
+        public DateTime BudgetRepappropriationDate { get; set; }
+        public int StatusId { get; set; }
+        public string Status { get; set; }
+        public DocumentStatus State { get; set; }
+        public IEnumerable<RemarksDto> RemarksList { get; set; }
+        public virtual List<BudgetReappropriationLinesDto> BudgetReappropriationLines { get; set; }
+        public bool IsAllowedRole { get; set; }
+
+    }
+}
---
+++ Application.Contracts/DTOs/BudgetReappropriation/CreateBudgetReappropriationLinesDto.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.DTOs
+{
+    public class CreateBudgetReappropriationLinesDto
+    {
+        public int? Id{ get; set; }
+        [Required]
+        public Guid? Level4Id { get; set; }
+        [Required]
+        public int? CampusId { get; set; }
+        [Required]
+        [MaxLength(500)]
+        public string Description { get; set; }
+        [Required]
+        [Range(0.00, int.MaxValue, ErrorMessage = "Please enter a value between greater than 0")]
+        public decimal AdditionAmount { get; set; }
+        [Required]
+        [Range(0.00, int.MaxValue, ErrorMessage = "Please enter a value between greater than 0")]
+        public decimal DeletionAmount { get; set; }
+    }
+}
