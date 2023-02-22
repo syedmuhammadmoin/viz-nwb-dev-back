4d9c6335b35fb72a55b72a38802b532576ffc528
zainnizami <Zain.Nizami@Ratechnologies.net>
2023-02-22 13:00:55 +0500

    Create new Specs DepreciationAdjustmentSpecs

---
+++ Infrastructure/Specifications/DepreciationAdjustmentSpecs.cs
@@ -0,0 +1,49 @@
+﻿using Application.Contracts.Filters;
+using Domain.Constants;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Specifications
+{
+    public class DepreciationAdjustmentSpecs : BaseSpecification<DepreciationAdjustmentMaster>
+    {
+        public DepreciationAdjustmentSpecs(TransactionFormFilter filter, bool isTotalRecord)
+        {
+            if (!isTotalRecord)
+            {
+                var validFilter = new PaginationFilter(filter.PageStart, filter.PageEnd);
+                ApplyPaging(validFilter.PageStart, validFilter.PageEnd - validFilter.PageStart);
+                AddInclude(i => i.Campus);
+                AddInclude(i => i.DepreciationAdjustmentLines);
+                AddInclude(i => i.Status);
+                AddInclude("DepreciationAdjustmentLines.FixedAsset");
+                AddInclude("DepreciationAdjustmentLines.Level4");
+                ApplyOrderByDescending(i => i.Id);
+            }
+        }
+        public DepreciationAdjustmentSpecs(bool forEdit)
+        {
+            if (forEdit)
+            {
+                AddInclude(i => i.DepreciationAdjustmentLines);
+                AddInclude(i => i.Status);
+            }
+            else
+            {
+                AddInclude(i => i.Campus);
+                AddInclude(i => i.DepreciationAdjustmentLines);
+                AddInclude(i => i.Status);
+                AddInclude("DepreciationAdjustmentLines.FixedAsset");
+                AddInclude("DepreciationAdjustmentLines.Level4");
+            }
+        }
+        public DepreciationAdjustmentSpecs() : base(e => (e.Status.State != DocumentStatus.Unpaid && e.Status.State != DocumentStatus.Partial && e.Status.State != DocumentStatus.Paid && e.Status.State != DocumentStatus.Draft && e.Status.State != DocumentStatus.Cancelled))
+        {
+            AddInclude(i => i.Status);
+        }
+    }
+}
