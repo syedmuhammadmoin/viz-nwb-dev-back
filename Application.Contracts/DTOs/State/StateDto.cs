e095552b16716e3e0b48e00626f03be6acf4a84e
Waleed Tariq <dev.ratech@outlook.com>
2023-03-30 05:37:38 +0500

    change name type to string

--- Application.Contracts/DTOs/State/StateDto.cs
+++ Application.Contracts/DTOs/State/StateDto.cs
@@ -3,7 +3,7 @@
     public class StateDto
     {
         public int Id { get; set; }
-        public int Name { get; set; }
+        public string Name { get; set; }
         public int CountryId { get; set; }
         public string Country { get; set; }
     }
