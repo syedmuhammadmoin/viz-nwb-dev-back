39e2ebafa455ed9eecbb75c6bdf8aa9484165cfc
Waleed Tariq <dev.ratech@outlook.com>
2023-04-19 16:37:35 +0500

    add id

--- Application.Contracts/DTOs/Batch/BatchLinesDto.cs
+++ Application.Contracts/DTOs/Batch/BatchLinesDto.cs
@@ -2,6 +2,7 @@
 {
     public class BatchLinesDto
     {
+        public int Id { get; set; }
         public int ProgramId { get; set; }
         public string Program { get; set; }
         public int MasterId { get; set; }
